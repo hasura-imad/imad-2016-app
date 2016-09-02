94f80fa6ff6f5e33bc44374e875d58ef0606b6b5
Tanmai Gopal <tanmaig@34cross.in>
2016-09-03 01:19:46 +0530

    Adds basic files

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('ui'));
+
+app.get('/', function (req, res) {
+  res.sendFile('./ui/index.html');
+});
+
+app.listen(80, function () {
+  console.log('IMAD course app listening on port 80!');
+});
