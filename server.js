a2305d6e6a2a06a04a010aa362ad497dd925f0f7
Coco <tanmaig@34cross.in>
2016-09-03 20:35:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static('ui'));
 
 app.get('/', function (req, res) {
-  res.sendFile('./ui/index.html');
+  res.sendFile('/app/index.html');
 });
 
 app.listen(80, function () {
