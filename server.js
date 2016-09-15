50b3bbb150301cf5a3fea3b2154244f19f973ff6
Tanmai Gopal <tanmaig@34cross.in>
2016-09-15 19:43:42 +0530

    Better directory structure

--- server.js
+++ server.js
@@ -6,8 +6,17 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
