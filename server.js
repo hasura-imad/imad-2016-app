48fb83a5473698f676f4fe329f334b929cb2415f
Tanmai Gopal <tanmaig@34cross.in>
2016-09-15 14:51:39 +0530

    Adds more dependencies to package.json and code now works on localhost

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
+var path = require('path');
+
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile('./app/index.html');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(80, function () {
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
   console.log('IMAD course app listening on port 80!');
 });
