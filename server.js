f726846b551b9438af2c188add9e9139a17a54ad
Coco <tanmaig@34cross.in>
2016-09-17 14:48:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+    res.send('Article one requested and will be served here');    
+});
+
+app.get('/article-two', function (req, res) {
+    res.send('Article two requested and will be served here');    
+});
+
+app.get('/article-three', function (req, res) {
+    res.send('Article three requested and will be served here');    
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
