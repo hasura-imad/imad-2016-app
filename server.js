c6730642f1cc8ea74380dff496461bf6008a1c37
akshatbhargava123 <akshatbhargavadeepa@gmail.com>
2016-09-17 22:38:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ app.get('/article-one', function (req,res) {
 });
 
 app.get('/article-two', function (req,res) {
-    res.send("Article two requested and will be served.");
+  res.sendFile(path.join(__dirname, 'ui', 'Article-two.html'));
 });
 
-app.get('/article-two', function (req,res) {
-    res.send("Article two requested and will be served.");
+app.get('/article-three', function (req,res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Article-three.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
