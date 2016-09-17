c93c6c956443a756cd3a617697763b14ddad05c4
S-Xync <saikumarimmadi@users.noreply.github.com>
2016-09-17 23:08:38 +0530

    Changes in the server.js

--- server.js
+++ server.js
@@ -12,7 +12,15 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+app.get('/article-one',function(req,res){
+  res.send("This is the page that serves article one");
+});
+app.get('/article-two',function(req,res){
+  res.send("This is the page that serves article two");
+});
+app.get('/article-three',function(req,res){
+  res.send("This is the page that serves artcile three");
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
