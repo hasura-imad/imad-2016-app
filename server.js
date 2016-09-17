9487a93de9e7a5a2df47a4b97d77f1fa3baa054a
luckyman000786 <luckyman000786@gmail.com>
2016-09-17 23:54:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,13 +4,43 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleOne = {
+    title:'Article One | Hemant Gupta',
+    heading:'Article One',
+    date:'Sep 21, 2017',
+    content:`<p>This is my first article and I am publishing it.This is my first article and I am publishing it.This is my first article and I am publishing it.This is my first article and I am publishing it.</p>
+    <p>This is my first article and I am publishing it.This is my first article and I am publishing it.This is my first article and I am publishing it.This is my first article and I am publishing it.</p>
+    <p>This is my first article and I am publishing it.This is my first article and I am publishing it.This is my first article and I am publishing it.This is my first article and I am publishing it.</p>`
+};
+function createTemplate (data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
 
+var htmlTemplate=`<html>
+<head><title>${title}</title>
+<link href="/ui/style.css" rel="stylesheet" />
+<meta name="viewport" content="width-device-width,initial-scale-1"/>
+</head>
+<body>
+   <div class ="conatiner">
+    <div>
+<div><a href = "/">Head to Home</a></div><hr/>
+<h3>${heading}</h3>
+
+<div>${date}</div>
+<div>${content}</div>
+</div>
+</div>
+</body>
+</html>`; return htmlTemplate;}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 app.get('/article-two', function (req, res) {
   res.send('ARTICLE TWO REQUESTED.');
