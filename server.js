b82759ee7716e5e3494488bd4013555fe70ea230
S-Xync <saikumarimmadi@users.noreply.github.com>
2016-09-18 01:06:05 +0530

    Fixes bug of articleName var not declared

--- server.js
+++ server.js
@@ -118,6 +118,7 @@ app.get('/ui/style.css', function (req, res) {
 	res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/:articleName',function(req,res){
+	var articleName=req.params.articleName;
 	res.send(createtemplate(articles[articleName]));
 });
 app.get('/ui/madi.png', function (req, res) {
