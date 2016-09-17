573fc9940ee2700d959bcc789fb3923d800b213b
S-Xync <saikumarimmadi@users.noreply.github.com>
2016-09-17 23:41:03 +0530

    Brings Articles into light

--- server.js
+++ server.js
@@ -6,27 +6,27 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+	res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+	res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/article-one',function(req,res){
-  res.send("This is the page that serves article one");
+	res.sendFile(path.join(__dirname,'ui','article-one.html'));
 });
 app.get('/article-two',function(req,res){
-  res.send("This is the page that serves article two");
+	res.sendFile(path.join(__dirname,'ui','article-two.html'));
 });
 app.get('/article-three',function(req,res){
-  res.send("This is the page that serves artcile three");
+	res.sendFile(path.join(__dirname,'ui','article-three.html'));
 });
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+	res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+	console.log(`IMAD course app listening on port ${port}!`);
 });
