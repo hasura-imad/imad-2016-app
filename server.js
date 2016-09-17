1ef0f200fc067d888a2bd006a99ca7866313fde6
luckyman000786 <luckyman000786@gmail.com>
2016-09-18 00:06:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -52,6 +52,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
+    var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
