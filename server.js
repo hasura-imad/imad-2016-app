ea20ceae1e3383f53f624828c376ee81c35a39a8
akshatbhargava123 <akshatbhargavadeepa@gmail.com>
2016-09-17 23:35:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,56 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var articleOne = {
+    title: 'Article One: Akshat',
+    heading: 'a',
+    date: 'Sept 5, 2016',
+    content: `
+            <p>
+                This is the content of article one.
+            </p>
+            <p>
+                This is the content of article one.
+                This is the content of article one.This is the content of article one.  
+            </p>
+            <p>
+                This is the content of article one.
+            </p>  `
+};
+
+var htmlTemplate = `
+
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name = "viewport" content = "width=device-width, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class = "head">
+            <div>
+                <a href = "/">Home</a>
+            <hr/>
+                <h1>
+                    ${heading}
+                </h1>
+                <div>
+                    ${date}
+                </div>
+            </div>
+        </div>
+        <div class = "container">
+            <div>
+                ${content}
+            </div>
+        </div>
+    </body>
+</html>
+
+`;
+
 var app = express();
 app.use(morgan('combined'));
 
