7fe2883c3bdd5690ab8740ac1db16a45cbec17c5
achyut <a0134598@u.nus.edu>
2016-09-18 13:03:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 app.get('/:articleName', function (req, res) {
 
-	var articleName = req.param.articleName;
+	var articleName = req.params.articleName;
 	res.send(createTemplate(articles[articleName]));
 });
 
