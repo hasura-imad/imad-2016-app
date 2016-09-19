164c6517ec8f4801425f7e887cbe12ad126f0ac5
Mariya <pkmariya@gmail.com>
2016-09-19 13:35:49 +0000

    added a new image file

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/Puppy.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Puppy.jpg'));
 });
 
 
