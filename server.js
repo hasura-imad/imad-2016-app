15dace493231afea26a818a0cd704caa46a59830
Gurubaran <tgurubaran@gmail.com>
2016-09-19 13:09:20 +0530

    My first branch changes

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
+var port = 8082; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8082, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
