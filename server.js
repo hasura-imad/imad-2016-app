d2d8dec24be9a6f07cff6c10a0a0ad5a493ce123
Isaac6171 <isaac4700@gmail.com>
2016-09-19 14:08:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/article-one',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 })
 app.get('/article-two',function(req,res){
-    res.send('Article two requested and will be served here');
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 })
 app.get('/article-three',function(req,res){
-    res.send('Article three requested and will be served here');
+     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 })
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
