fbf771eb0206b7bda0aaffa02c43626e849964f0
Vinit Kumar <vinitbhu06@gmail.com>
2016-09-19 12:31:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-one', function (req, res){
+    res.send('Article one will be resquested and served');
+});
+app.get('/article-two', function (req, res){
+    res.send('Article two will be resquested and served');
+});
+app.get('/article-three', function (req, res){
+    res.send('Article three will be resquested and served');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
