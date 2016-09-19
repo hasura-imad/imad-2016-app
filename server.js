2c0bdac748d9070277068eab9d0e7420d22a3557
Vinit Kumar <vinitbhu06@gmail.com>
2016-09-19 15:46:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
     
-    res.send(createTemplate(articles[articleName]), console.log('articleName'));
+    res.send(createTemplate(articles[articleName]) console.log('articleName'));
     
 });
 
