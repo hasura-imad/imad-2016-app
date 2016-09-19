3916e9f28c26d5c8e6eb2eeb08cdc98589a8b61a
Isaac6171 <isaac4700@gmail.com>
2016-09-19 17:49:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,19 +4,83 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleOne={
+    title:"Article one | Isaac Philip",
+    heading:"Article one",
+    date: "Sep 19, 2016",
+    content:`<p>
+               This is the content for my first article.This is the content for my first article. This is the content for my first article. 
+               This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+               This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+               This is the content for my first article. 
+                
+                
+              </p>
+              <p>
+               This is the content for my first article.This is the content for my first article. This is the content for my first article. 
+               This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+               This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+               This is the content for my first article. 
+                
+                
+            </p>`
+};
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+var htmlTemplate=
+    `<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+           <link href="/ui/style.css" rel="stylesheet" />
+       
+    </head>
+    <body>
+        <div class="container">
+        
+    <div>
+    <a href="/">Home</a>
+    </div>    
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        <div>
+            <img src="/C.V-01.png">
+        </div>
+        </div>
+        
+        
+        
+    </body>
+</html>
+ `;
+ return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-})
+   res.send(createTemplate(articleOne));
+});
 app.get('/article-two',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-})
+});
 app.get('/article-three',function(req,res){
      res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-})
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
