d2ee9419f34ee6aa0e90740ab7f083fc864f7600
krusty-crab <jakash1997@gmail.com>
2016-09-20 18:40:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article', function (req,res){
-	res.send('Article requested');
+app.get('/article-one', function (req,res){
+	 res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
