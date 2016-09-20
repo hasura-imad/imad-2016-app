4d42d7a7878863cd42bcbfad87781431c1027490
Coco <tanmaig@34cross.in>
2016-09-20 19:40:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
 // Move the image
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onclick = function () {
     img.style.marginLeft = '100px';
 };
