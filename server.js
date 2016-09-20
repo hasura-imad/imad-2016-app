e8b88c058a1a356622cc76c9bd95533ed5e47a86
Coco <tanmaig@34cross.in>
2016-09-20 13:04:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,6 +83,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+  counter = counter + 1;
+  res.send(counter.toString());
+});
+
 app.get('/:articleName', function (req, res) {
   // articleName == article-one
   // articles[articleName] == {} content object for article one
