c0f3fd1510d924b7f10dc757ac739ddfc53ccb06
Coco <tanmaig@34cross.in>
2016-09-22 01:06:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,14 +29,31 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-  // Make a request to the server and send the name
   
-  // Capture a list of names and render it as a list
-  var names = ['name1', 'name2', 'name3', 'name4'];
-  var list = '';
-  for (var i=0; i< names.length; i++) {
-      list += '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // Take some action
+          if (request.status === 200) {
+              // Capture a list of names and render it as a list
+              var names = request.responseText;
+              names = JSON.parse(names);
+              var list = '';
+              for (var i=0; i< names.length; i++) {
+                  list += '<li>' + names[i] + '</li>';
+              }
+              var ul = document.getElementById('namelist');
+              ul.innerHTML = list;   
+          }
+      }  
+      // Not done yet
+    };
+    
+    // Make the request
+    request.open('GET', 'http://coco98.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);  
+
 };
