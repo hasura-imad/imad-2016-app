04c0029f2d3891d8ad9649c570822b23910ad46d
S-Xync <saikumarimmadi@users.noreply.github.com>
2016-09-21 17:53:00 +0530

    Linked and Routed main.js

--- server.js
+++ server.js
@@ -118,6 +118,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
 	res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/main.js',function(req,res){
+	res.sendFile(path.join(__dirname,'ui','main.js'));
+});
 app.get('/:articleName',function(req,res){
 	var articleName=req.params.articleName;
 	res.send(createtemplate(articles[articleName]));
