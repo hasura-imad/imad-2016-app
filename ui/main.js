0fa2f9a6fe59583e25473b1866f5634040a42994
Coco <tanmaig@34cross.in>
2016-09-22 01:09:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,6 @@ button.onclick = function () {
 };
 
 // Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
   
@@ -53,6 +51,8 @@ submit.onclick = function () {
     };
     
     // Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET', 'http://coco98.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);  
 
