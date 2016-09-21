4b050ebffa745346317dc74496ae4df624f6f601
luckyman000786 <luckyman000786@gmail.com>
2016-09-21 21:44:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'Hello !! Welcome to HMG_007 Introduction Page';
 var image = document.getElementById('google');
 image.onclick = function () {
-    image.style.leftMargin = '100px';
+    image.style.marginLeft = '100px';
 };
