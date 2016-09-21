e534fd0f2b29c91e3408aa90767ca66d01aeade7
S-Xync <saikumarimmadi@users.noreply.github.com>
2016-09-21 18:35:01 +0530

    Makes the image move

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,14 @@
-console.log('Loaded!');
-alert('I\'m loaded');
+var mainText=document.querySelector("#main-text");
+function changeText() {
+  mainText.innerHTML="This is the new Text that is set from javascript file";
+}
+mainText.onclick=changeText;
+var mainImg=document.querySelector("#main-img");
+var margin=50;
+function moveImage(){
+  margin=margin+1;
+  mainImg.style.marginLeft=margin+'px';
+}
+mainImg.onclick=function(){
+  var interval=setInterval(moveImage, 5);
+};
