cb9e88cfdf047022eb2c7fb2771e1ccdc6196437
Coco <tanmaig@34cross.in>
2016-09-22 00:51:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,9 +109,9 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function(req, res) {
+app.get('/submit-name', function(req, res) { // URL: /submit-name?name=xxxxx
   // Get the name from the request
-  var name = req.params.name;
+  var name = req.query.name;
   
   names.push(name);
   // JSON: Javascript Object Notation
