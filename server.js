e2eaf3603bb42bbc93896c852ecf77d07926a204
luckyman000786 <luckyman000786@gmail.com>
2016-09-21 20:51:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,6 +59,9 @@ app.get('/:articleName', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
