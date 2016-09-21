e7141d42b2eb47c550c5766a6d659ddee4eefd20
Coco <tanmaig@34cross.in>
2016-09-22 00:14:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,20 @@ button.onclick = function () {
     request.open('GET', 'http://coco98.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+// Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+  // Make a request to the server and send the name
+  
+  // Capture a list of names and render it as a list
+  var names = ['name1', 'name2', 'name3', 'name4'];
+  var list = '';
+  for (var i=0; i< names.length; i++) {
+      list += '<li>' + names[i] + '</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+};
