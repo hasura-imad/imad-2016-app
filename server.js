195e5719411f792a24392add9c9b5b8c5472bb4c
achyut <a0134598@u.nus.edu>
2016-09-21 19:59:01 +0530

    client-side js

--- server.js
+++ server.js
@@ -84,10 +84,13 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 app.get('/:articleName', function (req, res) {
 
-	var articleName = req.param.articleName;
+	var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,16 @@
 console.log('Loaded!');
+alert('hello there!');
+
+var content = document.getElementById('main-content');
+var img = document.getElementById('madi');
+
+content.innerHTML = 'Changing content through javascript';
+
+var marginLeft = 0;
+function moveRight(){
+	marginLeft = marginLeft + 5;
+	img.style.marginLeft = marginLeft+'px';
+}
+img.onclick = function(){
+	setInterval(moveRight,50);
+}
