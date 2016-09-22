d0e9de0642bae4d26b0106b034e7cd2cdd15f836
suryan123 <suryanprasadom@gmail.com>
2016-09-22 19:19:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,11 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-  app.get('/article-one',  function (req, res) 
-  []
-    res.send('article one requested and will be served here'); 
-    } 
-    
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
