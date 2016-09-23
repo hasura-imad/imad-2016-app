a4745d04d93d19bdecac6b868724096d21b805de
S-Xync <saikumarimmadi@users.noreply.github.com>
2016-09-23 14:32:36 +0530

    Counter point has been made

--- server.js
+++ server.js
@@ -109,7 +109,11 @@ function createtemplate(data){
 	return htmlTemplate;
 
 }
-
+var counter=0;
+app.get('/counter',function(req,res){
+	counter=counter+1;
+	res.send(counter.toString());
+});
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'ui', 'index.html'));
