86f7ce163538485d92af78cda5257d4fa578b5e7
akshatbhargava123 <akshatbhargavadeepa@gmail.com>
2016-09-23 18:09:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
