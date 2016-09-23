f24cf10559e417d9bd31b6e279e39e7e0ab22c45
akshatbhargava123 <akshatbhargavadeepa@gmail.com>
2016-09-23 21:05:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,7 @@ button.onclick = function (){
 };
 
 // Submit Name Code
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_button');
 
 submit.onclick = function(){
@@ -50,6 +49,8 @@ submit.onclick = function(){
         }
         // else ignore request
     };
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET', "http://akshatbhargava123.imad.hasura-app.io/submit-name/:name", true);
     request.send(null);
     
