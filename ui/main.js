2fe828d609f9237dd162d6c6867c721d87f66b2d
akshatbhargava123 <akshatbhargavadeepa@gmail.com>
2016-09-23 20:19:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,8 +24,24 @@ button.onclick = function (){
     
 };
 
+// Submit Name Code
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_button');
 
-
+submit.onclick = function(){
+    // 1. Make a request to the server and send the name
+    
+    // 2. Capture a list of names and render it as a list
+    var name = ["name1", "name2", "name3"];
+    var list = '';
+    for(var  i = 0; i < names.length; i++){
+        list <= '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+    
+};
 
 
 // PRACTICE AREA
