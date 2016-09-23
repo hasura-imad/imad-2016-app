599a0a4fd2199bc40feba302c6b6c6be2b40d43b
akshatbhargava123 <akshatbhargavadeepa@gmail.com>
2016-09-23 18:28:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,12 @@ console.log('loaded');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'Hello :)';
+
+
+// MOVING MADI
+
+var image = document.getElementById('madi');
+
+image.onclick = function (){
+    image.style.marginLeft = '100px';
+};
