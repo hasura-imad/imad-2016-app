f3d75fb7431e5a86e8232785b5252de89f6dea35
georgekunchattil1992 <georgekunchattil@gmail.com>
2016-09-23 23:22:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.get('/article-one',function(req,res)
 
 app.get('/article-two',function(req,res)
 {
-   res.send('Article two requested and will be served here'); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
 });
 
 app.get('/article-three',function(req,res)
 {
-   res.send('Article three requested and will be served here'); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
 });
 
 app.get('/ui/style.css', function (req, res) {
