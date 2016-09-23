d9eb50b27719074bfe5d15613f94d02fe128df8d
akshatbhargava123 <akshatbhargavadeepa@gmail.com>
2016-09-23 20:52:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,6 +75,16 @@ app.get('/counter', function(req, res){
     res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name/:name', function(req, res){
+    // Get the name from the request
+    var name = req.params.name;
+    
+    names.push(name);
+    
+    res.send(JSON.stringify(names));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
