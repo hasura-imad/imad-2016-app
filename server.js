ca50c8e41cda5fa59c4546f2ea7c143699600aa9
Shaik Saif Ali <saifshine7@gmail.com>
2016-09-24 02:21:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,71 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne ={
+    title:'Article One|Saif Shines',
+    heading:'Article One',
+    date:'Sept 23 2016',
+    content: `
+     <div>
+            <p>
+                This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.
+            </p>
+              <p>
+                This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.
+            </p>
+              <p>
+                This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.
+            </p>
+              <p>
+                This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.
+            </p>
+        </div>
+    `
+    
+};
+
+function createTemple(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+var htmlTemplate = `
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, intial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+    <body>
+    <div class="container">
+        <div>
+            <a href="/">HOME</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+          ${content}
+        </div>
+    </div>    
+    </body>
+</html>
+';
+   `    return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));   
+ res.send(createTemplate(articleOne));  
     
 });
 app.get('/article-two',function(req,res){
