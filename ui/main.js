6e5302055b405993ce893cb73c376fd5a4156d75
Isaac6171 <isaac4700@gmail.com>
2016-09-23 13:14:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,12 +5,10 @@ element.innerHTML = 'New value';
 // move image
 var img = document.getElementById('madi');
 var marginLeft = 0;
-function move () {
-img.onclick = function(){
-    if(marginLeft===0){
-        marginLeft = MarginLeft +10;
-    
+function moveRight () {
+    marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
-    }
-};
 }
+img.onclick = function(){
+    var interval = setInterval(moveRight,50);
+};
