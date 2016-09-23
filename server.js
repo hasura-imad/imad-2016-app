302fb2d77b56aecad45afc1fd87bf114c04bd283
Shaik Saif Ali <saifshine7@gmail.com>
2016-09-24 00:00:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one',function(req,res){
+    res.send('Article one is requested, your request will be served shortly');   
+    
+});
+app.get('/article-two',function(req,res){
+   res.send('Article two is requested, your request will be served shortly');
+});
+app.get('/article-three',function(req,res){
+    res.send('Article three is requested, you request will be served shortly');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
