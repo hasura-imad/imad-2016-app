0e4f34612e6a10f65d247d8e27022d0bb59faac4
S-Xync <saikumarimmadi@users.noreply.github.com>
2016-09-23 15:04:00 +0530

    Counter is connected with XMLHttpRequest

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,16 @@
-var mainText=document.querySelector("#main-text");
-function changeText() {
-  mainText.innerHTML="This is the new Text that is set from javascript file";
-}
-mainText.onclick=changeText;
-var mainImg=document.querySelector("#main-img");
-var margin=50;
-function moveImage(){
-  margin=margin+1;
-  mainImg.style.marginLeft=margin+'px';
-}
-mainImg.onclick=function(){
-  var interval=setInterval(moveImage, 5);
+//code for the counter
+var counterButton=document.querySelector("#counter");
+counterButton.onclick=function(){
+  var request=new XMLHttpRequest();
+  request.onreadystatechange=function(){
+    if (request.readyState===XMLHttpRequest.DONE){
+      if (request.status===200) {
+        var counter=request.responseText;
+        var count=document.querySelector("#count");
+        count.innerHTML=counter.toString();
+      }
+    }
+  };
+  request.open('GET','http://localhost:8080/counter',true)
+  request.send(null);
 };
