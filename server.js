9454b9e223f7f563dd349897db3dddb8d47f1566
George Jacob <George Jacob>
2016-09-24 23:20:02 +0530

    updated server.js

--- server.js
+++ server.js
@@ -95,6 +95,17 @@ app.get('/counter',function(req,res){
     res.send(counter.toString());
 });
 
+var names=[];
+app.get('/submit-name',function(req,res){ //URL: /submit-name?name=xxxx (?name=xxxx--> query parameter string)
+   //Get the name from the request
+   var name=req.query.name;  //TODO
+
+   names.push(name);
+   //JSON Javascript Object Notation
+   res.send(JSON.stringify(names));
+});
+
+
 app.get('/:articleName',function(req,res)
 {
     //articleName=article-one
@@ -116,15 +127,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names=[];
-app.get('/submit-name',function(req,res){ //URL: /submit-name?name=xxxx (?name=xxxx--> query parameter string)
-   //Get the name from the request
-   var name=req.query.name;  //TODO
-
-   names.push(name);
-   //JSON Javascript Object Notation
-   res.send(JSON.stringify(names));
-});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
