2fdbe73845b00aa8391c19e2e0d19e7eaea5c2d9
georgekunchattil1992 <georgekunchattil@gmail.com>
2016-09-24 08:43:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,6 +93,7 @@ app.get('/:articleName',function(req,res)
 {
     //articleName=article-one
     //articles[articleName]={} content object for article-one
+    var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName])); 
 });
 
