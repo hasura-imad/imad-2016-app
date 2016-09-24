83738426a8e0067cd242fffb920c611efd710739
georgekunchattil1992 <georgekunchattil@gmail.com>
2016-09-24 11:10:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,13 @@ var element=document.getElementById('main-text'
 element.innerHTML='New value';
 
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+       marginLeft=marginLeft+10;
+       img.style.marginLeft=marginLeft+'px';
+    }
+    
     img.onclick=function(){
-       img.style.marginLeft="100px";     //-->embedded CSS feature
+       var interval=setInterval(moveRight,100);
+       
    };
