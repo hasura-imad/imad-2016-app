373b0e666d2b3d5780cbcb22e2d31544eb608463
enileda <adeline.andrews@gmail.com>
2016-09-24 15:17:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send("Article 1 is requested and will be served here");
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function (req, res) {
