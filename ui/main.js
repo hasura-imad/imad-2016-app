275958564929e6896ba68258f7c45d8991c56b4d
enileda <adeline.andrews@gmail.com>
2016-09-24 20:52:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,9 +7,16 @@ element.innerHTML = "Hello";
 // Move the image
 
 var img = document.getElementById('madi');
+var marginLeft=0;
+
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 
 img.onclick = function() {
-    img.style.marginLeft='200px';    
+    // img.style.marginLeft='200px';
+    var interval = setInterval(moveRight, 100);
 };
 
 
