417242340bb214542302326fd788db2a3e831a75
georgekunchattil1992 <georgekunchattil@gmail.com>
2016-09-24 14:02:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,6 +89,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+ 1;
+    res.send(counter.toString());
+});
+
 app.get('/:articleName',function(req,res)
 {
     //articleName=article-one
