f9c89015312d3f9f1c5108805a12f12f57be5d68
Mariya <pkmariya@gmail.com>
2016-09-24 07:38:24 +0000

    server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req, res) {
+  res.send("Article one requested and will be served here");
+
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
