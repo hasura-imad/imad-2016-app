12fdbce0ca45a5659699f5f1e5b57dcf49e0d6aa
Isaac6171 <isaac4700@gmail.com>
2016-09-24 13:53:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,15 +14,30 @@ img.onclick = function(){
 };
 // Counter code
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function(){
-    //make a request to the counterendpoint
+    //create a request object
+    var request = new XMLHttpRequest();
+    
     
     //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        
+        if(request.readyState === XMLHttpRequest.DONE){
+            //take some action
+            if(request.status === 200){
+                var counter = rerquest.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+  
+            }
+        }
+    };
+    //make the request
+    request.open('GET','http://isaac6171.imad.hasura-app.io/counter' , true);
+    request.send(null);
     
     //render the variable in the correct span
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+  
   
 };
