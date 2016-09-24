402870872aa9b96fbcb5df18d16c28f8527ed8d5
Mariya <pkmariya@gmail.com>
2016-09-24 18:47:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content = {
+  title: 'Article 1',
+  heading: 'Article One',
+  date: 'Sep 24, 2016',
+  content:
+  `   <p>This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+            </p>`
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
