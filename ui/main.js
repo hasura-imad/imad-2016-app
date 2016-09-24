4dec4ade04ab7fb7db17aa30d2086142ed502d31
George Jacob <George Jacob>
2016-09-24 19:26:41 +0530

    Create a request object

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var button=document.getElementById('counter');
 
 button.onclick=function(){
     
-    //Make a request to the counter endpoint
+    //Create a request object
     var request= new XMLHttpRequest();
 
     //Capture the response and store it in a variable
