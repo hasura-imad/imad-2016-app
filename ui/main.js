323a11db2aa5f3be6b40ea3bf28433352bae52f2
George Jacob <George Jacob>
2016-09-24 23:33:26 +0530

    render list of names

--- ui/main.js
+++ ui/main.js
@@ -11,9 +11,15 @@ button.onclick=function(){
     	if (request.readyState==XMLHttpRequest.DONE) {
     		//Take some action
     		if (request.status==200) {
-    			var counter=request.responseText;
-    			var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
+    			//Capture a list of names and render it as a list.
+	            var names=request.responseText;
+	            names=JSON.parse(names);
+	            var list='';
+	            for (var i=0;i<names.length;i++){
+		            list += '<li>' + names[i] + '</li>';
+	            }         
+	            var ul=document.getElementById('namelist'); //render list of names
+	            ul.innerHTML=list;
     		}
     	
     	}
@@ -21,7 +27,7 @@ button.onclick=function(){
     };
       
      //Make the request
-     request.open('GET','http://georgekunchattil1992.imad.hasura-app.io/counter',true);
+     request.open('GET','http://georgekunchattil1992.imad.hasura-app.io/submit-name?name',name,true);
      request.send(null);
 };
 
@@ -29,7 +35,7 @@ button.onclick=function(){
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
-submit.onclick=function(){
+submit.onclick=function() {
 	//Make a request to the server and send the name
 
 	//Capture a list of names and render it as a list.
