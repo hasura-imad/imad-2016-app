cab074de43ad9626f3337c1d54091f6fc82bbccc
Mariya <pkmariya@gmail.com>
2016-09-24 07:44:48 +0000

    server.js

--- server.js
+++ server.js
@@ -14,6 +14,16 @@ app.get('/article-one', function(req, res) {
 
 });
 
+app.get('/article-two', function(req, res) {
+  res.send("Article two requested and will be served here");
+
+});
+
+app.get('/article-three', function(req, res) {
+  res.send("Article three requested and will be served here");
+
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
