3af5c044d0cbcf7adf1bdc181765ec6b3e1477b1
Mariya <pkmariya@gmail.com>
2016-09-24 18:58:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -52,6 +52,8 @@ var htmlTemplate =
     </body>
 
     </html>`;
+    
+    return htmlTemplate;
 }
 
 app.get('/', function (req, res) {
@@ -59,7 +61,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function(req, res) {
