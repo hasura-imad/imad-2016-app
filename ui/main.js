29876a0eac81ac91158d9a9e5b17894536f903f3
George Jacob <George Jacob>
2016-09-24 21:52:58 +0530

    Change code from localhost to 'imad.hasura-app.io' server

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ button.onclick=function(){
     };
       
      //Make the request
-     request.open('GET','http://localhost:8080/counter',true);
+     request.open('GET','http://georgekunchattil1992.imad.hasura-app.io/counter',true);
      request.send(null);
 };
 
@@ -31,5 +31,14 @@ var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
 	//Make a request to the server and send the name
+
 	//Capture a list of names and render it as a list.
+	var names=['name1','name2','name3','name4'];
+	var list='';
+	for (var i=0;i<names.length;i++){
+		list+='<li>'+names[i] + '</li>';
+	}
+	var ul=document.getElementById('namelist');
+	ul.innerHTML=list;
+	}
 };
