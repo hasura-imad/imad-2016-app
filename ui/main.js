b8dc77aeba930c66d18432a4d7ea544f6a9e99ff
George Jacob <George Jacob>
2016-09-24 22:06:20 +0530

    updated main.js

--- ui/main.js
+++ ui/main.js
@@ -36,9 +36,8 @@ submit.onclick=function(){
 	var names=['name1','name2','name3','name4'];
 	var list='';
 	for (var i=0;i<names.length;i++){
-		list+='<li>'+names[i] + '</li>';
+		list += '<li>' + names[i] + '</li>';
 	}
 	var ul=document.getElementById('namelist');
 	ul.innerHTML=list;
-	}
 };
