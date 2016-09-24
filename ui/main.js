0d9227ad7e3776d54844a6eac7cf4b6863c8373f
George Jacob <George Jacob>
2016-09-24 23:43:12 +0530

    rendering list updated in main.js

--- ui/main.js
+++ ui/main.js
@@ -27,13 +27,14 @@ button.onclick=function(){
     };
       
      //Make the request
+     var nameInput=document.getElementById('name');
+     var name=nameInput.value;
      request.open('GET','http://georgekunchattil1992.imad.hasura-app.io/submit-name?name',name,true);
      request.send(null);
 };
 
 //Submit name
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function() {
 	//Make a request to the server and send the name
