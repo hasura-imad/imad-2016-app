477c1ebf8cc3a5409cf8a326807e14bda11a9361
georgekunchattil1992 <georgekunchattil@gmail.com>
2016-09-24 07:57:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName',function(req,res)
+app.get('/article-one',function(req,res)
 {
     res.send(createTemplate(articleOne)); 
 });
