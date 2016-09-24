8c050858832a866e3a51870a1b3c0ce18c9c523b
Mariya <pkmariya@gmail.com>
2016-09-24 18:54:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,6 +14,38 @@ var articleOne = {
             </p>`
 };
 
+var htmlTemplate = 
+  `<html>
+    
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <div>
+            <h3>
+                ${heading}
+            </h3>
+        </div>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+    </body>
+
+</html>`;
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
