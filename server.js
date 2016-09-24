a519cf977763a34adf8136caf087afa1dfa8ee1c
enileda <adeline.andrews@gmail.com>
2016-09-24 14:52:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var morgan = require('morgan');
+var express = require('express'); //Create the web server
+var morgan = require('morgan'); // Output logs
 var path = require('path');
 
 var app = express();
