cb9cbfb8f7a04d6372b55f6704f2fcc7903241a3
Shaik Saif Ali <saifshine7@gmail.com>
2016-09-25 09:44:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,92 +1,122 @@
-var express = require('express'); //express is the keyword, to create a webserver so that we don't have to set listhn etc
+var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne ={
-    title:'Article One|Saif Shines',
-    heading:'Article One',
-    date:'Sept 23 2016',
-    content: `
-     <div>
-            <p>
-                This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.
-            </p>
-              <p>
-                This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.
-            </p>
-              <p>
-                This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.
-            </p>
-              <p>
-                This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.This is my first Para,boss.
-            </p>
-        </div>
-    `
-    
+
+var articles = {
+    'article-one': {
+      title: 'Article One | Tanmai Gopal',
+      heading: 'Article One',
+      date: 'Sep 5, 2016',
+      content: `
+          <p>
+              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+          </p>
+          <p>
+              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+          </p>
+          <p>
+              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+          </p>`
+    },
+    'article-two': {
+      title: 'Article Two | Tanmai Gopal',
+      heading: 'Article Two',
+      date: 'Sep 10, 2016',
+      content: `
+          <p>
+              This is the content for my second article.
+          </p>`
+    },
+    'article-three': {
+      title: 'Article Three | Tanmai Gopal',
+      heading: 'Article Three',
+      date: 'Sep 15, 2016',
+      content: `
+          <p>
+              This is the content for my third article.
+          </p>`
+    }
 };
 
-function createTemple(data){
+function createTemplate (data) {
     var title = data.title;
-    var heading = data.heading;
     var date = data.date;
+    var heading = data.heading;
     var content = data.content;
-var htmlTemplate = `
-<html>
-    <head>
-        <title>
-            ${title}
-        </title>
-        <meta name="viewport" content="width=device-width, intial-scale=1" />
-        <link href="/ui/style.css" rel="stylesheet" />
-        </head>
-    <body>
-    <div class="container">
-        <div>
-            <a href="/">HOME</a>
-        </div>
-        <hr/>
-        <h3>
-            ${heading}
-        </h3>
-        <div>
-            ${date}
-        </div>
-        <div>
-          ${content}
-        </div>
-    </div>    
-    </body>
-</html>
-';
-   `    return htmlTemplate;
+    
+    var htmlTemplate = `
+    <html>
+      <head>
+          <title>
+              ${title}
+          </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="/ui/style.css" rel="stylesheet" />
+      </head> 
+      <body>
+          <div class="container">
+              <div>
+                  <a href="/">Home</a>
+              </div>
+              <hr/>
+              <h3>
+                  ${heading}
+              </h3>
+              <div>
+                  ${date}
+              </div>
+              <div>
+                ${content}
+              </div>
+          </div>
+      </body>
+    </html>
+    `;
+    return htmlTemplate;
 }
-
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res){
- res.send(createTemplate(articleOne));  
-    
+
+var counter = 0;
+app.get('/counter', function (req, res) {
+   counter = counter + 1;
+   res.send(counter.toString());
 });
-app.get('/article-two',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));   
+
+var names = [];
+app.get('/submit-name', function(req, res) { // /submit-name?name=xxxx
+  // Get the name from the request
+  var name = req.query.name;
+  
+  names.push(name);
+  // JSON: Javascript Object Notation
+  res.send(JSON.stringify(names));
 });
-app.get('/article-three',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));   
+
+app.get('/:articleName', function (req, res) {
+  // articleName == article-one
+  // articles[articleName] == {} content object for article one
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
