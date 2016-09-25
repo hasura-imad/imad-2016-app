33f15c915deb87ab1e5161aaa3f00d37e86f3b69
krusty-crab <jakash1997@gmail.com>
2016-09-25 10:13:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
 // counter code
-var button = getElementById('counter');
+var button = getElementById("counter");
 var counter=0;
 button.onclick = function () {
     // rendering correct count
     counter+=1;
-    var span = document.getElementById('count');
+    var span = document.getElementById("count");
     span.innerHTML= counter.toString();
 };
