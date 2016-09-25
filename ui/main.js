5ffdacc03733d4b365b75810606d64538f1355a7
krusty-crab <jakash1997@gmail.com>
2016-09-25 12:29:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,17 @@
 var button = document.getElementById('counter');
 var counter=0;
 button.onclick = function () {
+    /*// make request to counter enddpoint
+    var request = new XMLHttpRequest();
+    
+    // catch request
+    request.onreadystatechange = function () {
+      if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === "200"){
+                
+            }
+        }
+    };*/
     // rendering correct count
     counter=counter+1;
     var span = document.getElementById('count');
