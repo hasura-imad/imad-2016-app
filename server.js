5580157ef0cbc7349cf22b27002e966f5eca8a85
Isaac6171 <isaac4700@gmail.com>
2016-09-25 17:09:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,8 +134,8 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('ui/photo_opt.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'photo_opt.jpg'));
 });
 
 
