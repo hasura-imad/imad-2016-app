d0988bef1c2afcc1d4d7caab51fa28fce2304b3b
Mariya <pkmariya@gmail.com>
2016-09-25 12:30:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+
+var img = document.getElementById("myimage");
+var marginLeft = 0;
+
+function moveRight() {
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + "px";
+}
+
+img.onclick = function ()
+{
+    var interval = setInterval(moveRight, 50);
+}
