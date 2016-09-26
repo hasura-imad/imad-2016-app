2ad940748c0622deb8a5d55491387d1903b97390
Isaac6171 <isaac4700@gmail.com>
2016-09-26 15:36:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,4 @@
-[console.log('Loaded!');
-// Change the text of the main-text div
-var element = document.getElementById('main-text');
-element.innerHTML = '';
-// move image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight,10);
-};
-]
+
 // Counter code
 var button = document.getElementById('counter');
 
