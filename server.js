86ce54c74a9a948bfae2d07eca2efcfc2988b99d
Isaac6171 <isaac4700@gmail.com>
2016-09-26 13:26:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,8 +139,8 @@ app.get('/ui/photo_opt.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'photo_opt.jpg'));
 });
 var names = [];
-app.get('/submit-name/:name', function(req,res){
-    var name = req.params.name;
+app.get('/submit-name', function(req,res){
+    var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 }
