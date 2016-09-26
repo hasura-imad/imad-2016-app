c87d8b5153c30e991db493f808076a52e7770a9f
Vinit Kumar <vinitbhu06@gmail.com>
2016-09-26 14:45:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var bootstrap = require('bootstrap');
+//var bootstrap = require('bootstrap');
 
 var app = express();
 app.use(morgan('combined'));
