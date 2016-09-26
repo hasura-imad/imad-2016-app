1d2dc6857f36c1c74e5e83827b4c1d9bab038b3b
vineth17 <vineth17@gmail.com>
2016-09-26 11:06:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req,res) {
+  res.send("The article one will be served");       
+});
+
+app.get('/article-one', function(req,res) {
+  res.send("The article two will be served");       
+});
+
+app.get('/article-one', function(req,res) {
+  res.send("The article three will be served");       
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
