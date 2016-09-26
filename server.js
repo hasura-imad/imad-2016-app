a252f6eb7b7770e783ed2dd78a925ae6e4da5ceb
Isaac6171 <isaac4700@gmail.com>
2016-09-26 12:40:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.get('/', function (req, res) {
 var counter = 0;
 app.get('/counter',function(req,res){
    counter = counter + 1;
-res.send(counter.toString());
+   res.send(counter.toString());
 });
 app.get('/:articleName',function(req,res){
     var articleName = req.params.articleName;
