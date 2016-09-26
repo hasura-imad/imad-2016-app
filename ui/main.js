ccee19eca77926ebaf155a26718a835a4d344d4f
Isaac6171 <isaac4700@gmail.com>
2016-09-26 13:11:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -48,4 +48,15 @@ button.onclick = function(){
      
      
      
+     //example
+     var names =['name1','name2','name3'];
+     var list = '';
+     for(var i=0;i<names.length;i++){
+         list += '<li>'+ names[i]+'</li>';
+     }
+     var ul = document.getElementById('namelist ');
+     ul.innerHtml = list;
+     
+     
+     
  };
