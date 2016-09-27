51b141280c8103e198d72f2f89a396ff4d10c882
vineth17 <vineth17@gmail.com>
2016-09-27 14:43:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,6 +87,10 @@ app.get('/:articleName', function(req,res) {
 });
 
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
