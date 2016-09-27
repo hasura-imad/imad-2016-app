a515ef4e77e4edd9bf809d322849f192ce93e4b1
vineth17 <vineth17@gmail.com>
2016-09-27 15:00:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,11 @@
 console.log('Loaded!');
 //move the image
 var img = document.getElementById('logo');
+function moveRight() {
+    marginLeft = marginLeft = 1;
+    pmg.style.marginLeft = marginLeft = 'px';
+}
 img.onClick = function() {
     img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight,5) ;
 };
