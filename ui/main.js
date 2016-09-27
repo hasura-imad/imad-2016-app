0006ab82c7bf6bdd020b32e74d566daa47b42b40
vineth17 <vineth17@gmail.com>
2016-09-27 15:53:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 //move the image
-var img = document.getElementById('logo');
+var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight() {
     marginLeft = marginLeft = 1;
