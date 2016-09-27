61a7635ee5eecc99ad9040de2f2fb05919c415d3
shagufta12 <shaguftaanjum14@gmail.com>
2016-09-27 08:44:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/ui/style.css', function (req, res) {
