a56e01c9e7ff02ffb93918138e8cd0fb6d2b4ad7
anitamahotra63 <anitamahotra63@gmail.com>
2016-09-28 18:52:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
-app.get('/article-one',function(req,res){
-   res.send("article one is requested here."); 
+app.get('/article-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
+
 
 app.get('/article-two',function(req,res){
    res.send("article two is requested here."); 
