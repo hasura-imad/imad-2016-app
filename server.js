c213b1b020ffe8e11c8992dc50b5e999a4852950
vineth17 <vineth17@gmail.com>
2016-09-28 14:11:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,18 +81,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter = 0; 
-app.get('/counter', function (req,res) {
-    counter = counter + 1;
-    res.send(counter.toString());
-});
-
 var names = [];
 app.get('/submit-name', function(req,res) {
     var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
+
+var counter = 0; 
+app.get('/counter', function (req,res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
 
 app.get('/:articleName', function(req,res) {
     var articleName = req.params.articleName;
