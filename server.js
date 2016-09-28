dc98692ac8576d31aee62c294f1a72ca4be1a611
rajatsarkari <rajatsarkari777@gmail.com>
2016-09-28 23:42:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send("Article One is requested and will be served shortly");
+});
+
+app.get('/article-two', function (req, res) {
+  res.send("Article Two is requested and will be served shortly");
+});
+
+app.get('/article-three', function (req, res) {
+  res.send("Article Three is requested and will be served shortly");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
