e18a2c60d93e9558fe7551a0541801fdd9a20ad2
vineth17 <vineth17@gmail.com>
2016-09-28 14:16:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get('/', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name', function(req,res) {
+app.get('/submit-name/:name', function(req,res) {
     var name = req.params.name;
     names.push(name);
     res.send(JSON.stringify(names));
