91e6ee15ef95389c1e1c6d697c0a239a039c6a81
vineth17 <vineth17@gmail.com>
2016-09-28 12:37:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,8 +18,6 @@ button.onclick = function() {
         
     };
     
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-    
+    request.open('GET','http://vineth17.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
