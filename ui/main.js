b02d5fcd5adcee680a486f4d24c7201741c81122
suryan123 <suryanprasadom@gmail.com>
2016-09-28 18:26:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,10 +8,8 @@
      //Capture the response and store it in a variable
      
      //Render the variable in the correct span
-     
-     counter=counter+1;
-     counter=0;
+      counter=counter+1;
+      counter=0;
       var span=document.getElementById('count');
       span.innerHTML=counter.toString();
-      
  };
