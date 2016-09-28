23173f629da97e5b3ad398dc6caf6d31304407fa
vineth17 <vineth17@gmail.com>
2016-09-28 14:07:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,6 +87,13 @@ app.get('/counter', function (req,res) {
     res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name', function(req,res) {
+    var name = req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName', function(req,res) {
     var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName]));
