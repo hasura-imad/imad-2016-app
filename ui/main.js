b54f440a36134d688af4758e3195fc7ff4b79b4e
vineth17 <vineth17@gmail.com>
2016-09-28 12:31:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,22 @@
 //counter
 var button = document.getElementById('counter');
-var counter = 1;
+
 
 button.onclick = function() {
+    //request to endpoint
+    var request = new XMLHttpRequest();
+    
+    //capture the state
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        
+    };
     
     counter = counter + 1;
     var span = document.getElementById('count');
