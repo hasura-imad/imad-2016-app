5d9d9a0a0a3dd1ded5a310e46e0db989d2d0ae20
vineth17 <vineth17@gmail.com>
2016-09-28 13:38:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,7 +32,7 @@ submit.onclick = function() {
     var list = '';
     for (var i=0; i< names.length; i++){
          
-        list =  names[i] ;
+        list == '<li>' + names[i] + '</li>';
     }
     alert(list);
     var ul = document.getElementById('namelist');
