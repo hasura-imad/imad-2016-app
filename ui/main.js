ecc7e0213044f3e057f7de233f0826db73e43e82
enileda <adeline.andrews@gmail.com>
2016-09-28 21:44:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,5 @@
 console.log('Loaded!');
 
-// Change the displayed text
-var element = document.getElementById("main-text");
-element.innerHTML = "Hello";
-
 // Move the image
 
 var img = document.getElementById('madi');
