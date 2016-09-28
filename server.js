a84d15715390cfc15c069889f343a4c5b35040b5
suryan123 <suryanprasadom@gmail.com>
2016-09-28 17:56:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+        var counter = 0;
+        app.get('/counter', function (req,res){
+        counter=counter+1;
+        res.send(counter.toString());
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
