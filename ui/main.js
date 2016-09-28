0121cc26fe9f8b9043e6bf3ede9f038d3f9ef54b
vineth17 <vineth17@gmail.com>
2016-09-28 15:57:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,31 +23,31 @@ button.onclick = function() {
 };
 
 //submit request
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
-      var request = new XMLHttpRequest();
-    
+var request = new XMLHttpRequest();
     //capture the state
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200){
-                var names= ['name1','name2','name3','name4'];
-    var list = '';
-    for (var i=0; i< names.length; i++){
-         
-        list += '<li>' + names[i] + '</li>';
-    }
-    alert(list);
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+                var names= request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (var i=0; i< names.length; i++){
+                     
+                    list += '<li>' + names[i] + '</li>';
+                }
+                alert(list);
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
             }
         }
         
     };
-    
-    request.open('GET','http://vineth17.imad.hasura-app.io/submit-name' + name,true);
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET','http://vineth17.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
 
    
