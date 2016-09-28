8474331c25470ab33dc48e38d0951d2d3c18f078
enileda <adeline.andrews@gmail.com>
2016-09-28 21:48:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,6 +85,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
