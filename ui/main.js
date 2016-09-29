de4a5dc9e80d46a93e5504ab45266cdb4f98f2e3
suryan123 <suryanprasadom@gmail.com>
2016-09-29 18:28:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,3 +27,30 @@
           
         };
         
+        // Submit name
+        
+        var nameInput = document.getElementById('name');
+        var name = nameInput.value;
+        var submit = document.getElementById('sibmit_btn');
+        submit.onclick = function () {
+            
+            // Make a request to the server and send the name
+            
+            // Capture a list of names and render it as a list.
+            
+            var names = ['name1','name2','name3','name4'];
+            var list ='';
+            for (var i=0; i<names.length; i++){
+            list += '<liv' + names[i] + '</liv';
+            }
+            
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
+        };
+        
+            
+            
+        
+        }
+        
+        
