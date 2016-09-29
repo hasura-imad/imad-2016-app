06cdd54183fc0ee4f3e7bf1d10ef2c0317f295ce
ramit0402 <ramitlegend@gmail.com>
2016-09-29 23:30:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/styles.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'styles.css'));
+});
+
 app.get('/ui/logo.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'logo.png'));
 });
