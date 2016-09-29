c1a31b1f7e857d4e6b1a4c1a92ee58455c2a3f89
PRASANNARAGAV <vayujeevotamaha29@gmail.com>
2016-09-29 23:16:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-     res.sendFile(path.join(__dirname, 'ui','Article-one'));
+     res.sendFile(path.join(__dirname, 'ui','Article-one.html'));
   
 });
 
