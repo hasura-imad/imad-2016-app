f18956c8b1bdf9267f28c381348493f62ac1d478
ramit0402 <ramitlegend@gmail.com>
2016-09-29 23:28:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/signup', function (req, res) {
+app.get('/signup.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'signup.html'));
 });
 
