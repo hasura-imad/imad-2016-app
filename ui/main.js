001047bbd7f6b76af364c123004660ad78eb4c7a
rajatsarkari <rajatsarkari777@gmail.com>
2016-09-29 19:21:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 
 var img = document.getElementById("madi");
 var marginleft = 0;
-function moveright () {
+img.onclick = function() {
     img.style.marginLeft = "100px";
 }
