bc2242af9be00a5f8eeaa4d9b364ba39dacfccf6
rajatsarkari <rajatsarkari777@gmail.com>
2016-09-29 18:42:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,6 +87,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
