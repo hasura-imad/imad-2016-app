eaa0736f022df9b946e722495e36dadfdc4cb1e3
PRASANNARAGAV <vayujeevotamaha29@gmail.com>
2016-09-29 22:37:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -18,7 +18,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/ARTICLE-ONE', function (req, res) {
-  res.send('Article one in progress');
+  res.sendFile(path.join(__dirname, 'ui', 'ARTICLE-ONE.html'));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
