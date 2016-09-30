afe3cd3180ce974f761cc3d3e279e2eafd73b830
Mariya <pkmariya@gmail.com>
2016-09-30 09:35:23 +0000

    counter methods

--- ui/main.js
+++ ui/main.js
@@ -15,10 +15,22 @@ img.onclick = function ()
 */
 
 // Counter Code
-var counter = 0;
+
 var button = document.getElementById('counter');
+
 button.onclick = function() {
-	counter = counter + 1;
-	var span = document.getElementById('count');
-	span.innerHTML = counter.toString();
+	var request = new XMLHttpRequest();
+
+	request.onreadystatechange = function() {
+		if(request.readyState === XMLHttpRequest.DONE){
+			if(request.status === 200) {
+				var counter = request.responseText;
+				var span = document.getElementById('count');
+				span.innerHTML = counter.toString();
+			}
+		}
+	};
+	
+	request.open('GET', 'http://pkmariya.imad.hasura-app.io/counter', true);
+	request.send(null);
 };
