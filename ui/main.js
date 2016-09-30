1ab87cc8a60fdd2e2483b717e4258f29ccaa24d2
Mariya <pkmariya@gmail.com>
2016-09-30 09:21:17 +0000

    counter changes

--- ui/main.js
+++ ui/main.js
@@ -19,6 +19,6 @@ var counter = 0;
 var button = document.getElementById('counter');
 button.onclick = function() {
 	counter = counter + 1;
-	var span = document.getElementById('span');
+	var span = document.getElementById('count');
 	span.innerHTML = counter.toString();
 };
