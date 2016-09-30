386784ad2d9b64ea200588306ee63ca7705e6aad
Rahul Mahanot <rahulmahanott08@gmail.com>
2016-09-30 23:17:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
-//counter code
-var button= document.getElementById('counter');
+// counter code
+var button = document.getElementById('counter');
 var counter=0;
-button.onclick =function() {
-    counter=counter+1;
+button.onclick = function () {
+    counter = counter + 1;
     var span= document.getElementById('count');
     span.innerHTML = counter.toString();
 };
