220a0db8a05cd0e24af2e47d971a59b08c7aa5af
Rahul Mahanot <rahulmahanott08@gmail.com>
2016-09-30 22:51:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 var counter=0;
 app.get('/counter',function(req,res){
     counter=counter+1;
-    res.send(counter.tostring());
+    res.send(counter.toString());
     
 });
 
