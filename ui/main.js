bd05c0f2aa3e053c0818f85de30f826e106b66b7
Rahul Mahanot <rahulmahanott08@gmail.com>
2016-09-30 23:08:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
-console.log('Loaded!');
+//counter code
+var button= documnet.getElementById('counter');
+var counter=0;
+button.onclick =function() {
+    counter=counter+1;
+    var span= document.getElemenById('count');
+    span.innerHTML = counter.toString();
+};
