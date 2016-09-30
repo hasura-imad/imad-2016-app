7135ba74c4126f5d18d1579fd25f3bea74f6ff6c
Mariya <pkmariya@gmail.com>
2016-09-30 17:23:39 +0000

    names list addition

--- ui/main.js
+++ ui/main.js
@@ -34,3 +34,16 @@ button.onclick = function() {
 	request.open('GET', 'http://pkmariya.imad.hasura-app.io/counter', true);
 	request.send(null);
 };
+
+var nameInput = document.getElementById('name').value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function (){
+	var names = ['name1', 'name2', 'name3'];
+	var list = '';
+
+	for(var i=0; i<names.length; i++)
+		list += '<li>' + names[i] + '</li>';
+
+	var ul = document.getElementById('namelist');
+	ul.innerHTML = list;
+};
