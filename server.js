a3aa02d0b53accf6edcb4c5dedbad13ec081e82f
Mariya <pkmariya@gmail.com>
2016-09-30 08:59:38 +0000

    added counter

--- server.js
+++ server.js
@@ -87,6 +87,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get("/counter", function(req, res) {
+	counter = counter + 1;
+	res.send(counter.toString());
+});
 app.get('/:articleName', function(req, res) {
 //    res.send(createTemplate(articleOne));
     var articleName = req.params.articleName;
