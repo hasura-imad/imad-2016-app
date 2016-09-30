53806c6342be57a98db1c9a489e2a1505a364caa
Mariya <pkmariya@gmail.com>
2016-09-30 17:41:28 +0000

    added JSON

--- server.js
+++ server.js
@@ -110,6 +110,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+var names = [];
 app.get('/submit-name/:name', function(req, res) {
 	var name = req.params.name;
 	names.push(name);
