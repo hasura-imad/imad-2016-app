75b28646b8452dde4d273c63f1c4833953a572e5
Mariya <pkmariya@gmail.com>
2016-10-01 01:32:11 +0000

    some changes

--- ui/main.js
+++ ui/main.js
@@ -35,15 +35,19 @@ button.onclick = function() {
 	request.send(null);
 };
 
-var nameInput = document.getElementById('name').value;
+var nameInput;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function (){
 	var names = ['name1', 'name2', 'name3'];
 	var list = '';
 
-	for(var i=0; i<names.length; i++)
-		list += '<li>' + names[i] + '</li>';
+	nameInput = document.getElementById('name').value;
 
+	for(var i=0; i<names.length; i++)
+	{
+		names[i] = nameInput;
+		list += '<li>' + names[i] + '</li>';
+	}
 	var ul = document.getElementById('namelist');
 	ul.innerHTML = list;
 };
