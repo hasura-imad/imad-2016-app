3c43779f3e310ab8651cd18a490eafd15c395040
Mariya <pkmariya@gmail.com>
2016-10-01 02:44:12 +0000

    added favicon req

--- server.js
+++ server.js
@@ -117,6 +117,10 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/ui/favicon.ico', function(req, res) {
+	res.sendFile(path.join(__dirname, 'ui', 'favicon.ioc'));
+}
+
 var port = 8081; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8081, function () {
   console.log(`IMAD course app listening on port ${port}!`);
