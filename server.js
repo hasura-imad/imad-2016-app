c1e07eab8d130c6121eadacdf90de1b4c077dd77
Mariya <pkmariya@gmail.com>
2016-10-01 14:54:45 +0000

    port changes

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.get('/ui/favicon.ico', function(req, res) {
 	res.sendFile(path.join(__dirname, 'ui', 'favicon.ioc'));
 });
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
+var port = 80; // Use 8080 for local development because you might already have apache running on 80
+app.listen(80, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
