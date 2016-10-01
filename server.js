a99bf143cefd856f23d34509bba59a0591d00fcc
georgekunchattil1992 <georgekunchattil@gmail.com>
2016-10-01 22:50:21 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(morgan('combined'));
 app.get('/ui/index.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app.get('/ui/index.html', function(req, res){
+  res.sendfile(__dirname + 'ui/index.html');
+});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
