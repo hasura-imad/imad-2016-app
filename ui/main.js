9f7257c90d8efd053f8d934305e47edd6c7c1e6b
luckyman000786 <luckyman000786@gmail.com>
2016-10-01 21:33:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,3 +9,15 @@ function moveRight() {
 
 image.onclick = function() {
 var interval = setInterval(moveRight,100);};
+
+var button = document.getElementById('but1');
+button.onclick = function(){
+var request = new XMLHttpRequest();  
+reqauest.onreadystatechange = function(){if(request.readyState===XMLHttpRequest.DONE){if(request.status ===200){var counter = request.responseText;
+ var span = document.getElementById('zero');
+ span.innerHTML = counter.toString();
+    
+}}};
+request.open('GET','http://luckyman000786.imad.hasura-app.io/counter',true);
+request.send(null);
+};
