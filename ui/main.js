97216478f2b65bcb4cc10f29f7ba7d2d92fed115
rajatsarkari <rajatsarkari777@gmail.com>
2016-10-01 16:23:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,13 +22,28 @@ button.onclick = function(){
 var nameInput = document.getElementById('name'); 
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
-submit.onclick = function(){
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i = 0;i<names.length;i++)
-    {
-        list += '<li>' + names[i] + '</li>';
+submit.onclick = function(){ //create a request to the counter endpoint
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i = 0;i<names.length;i++)
+                {
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');   
+                ul.innerHTML = list;
+            }
+        }
+        
     }
-var ul = document.getElementById('namelist');   
-ul.innerHTML = list;
+    //Render the variable in the correct span
+    request.open('GET', 'http://rajatsarkari.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+    
 }
