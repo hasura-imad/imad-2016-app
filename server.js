5ff3f7ece5ddd98f32272b83516f06f02adf78a6
rajatsarkari <rajatsarkari777@gmail.com>
2016-10-01 14:28:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,6 +78,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+  res.send(counter.toString());
+});
+
 app.get('/:pageName', function (req, res) {
     var pageName = req.params.pageName;
     res.send(createtemplate(pages[pageName]));
