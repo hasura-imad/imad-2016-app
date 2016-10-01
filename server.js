7e899ae98b257ac2c3efe7bc3d350aee19f2482d
Mariya <pkmariya@gmail.com>
2016-10-01 02:46:53 +0000

    error fix

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/ui/favicon.ico', function(req, res) {
 	res.sendFile(path.join(__dirname, 'ui', 'favicon.ioc'));
-}
+});
 
 var port = 8081; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8081, function () {
