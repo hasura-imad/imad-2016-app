2eeca4b1ccdcc9eee7c9f1205d46aa205afc2eae
Mariya <pkmariya@gmail.com>
2016-10-01 15:19:11 +0000

    port changes

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.get('/', function (req, res) {
 });
 
 var counter = 0;
-app.get("/counter", function(req, res) {
+app.get('/counter', function(req, res) {
 	counter = counter + 1;
 	res.send(counter.toString());
 });
