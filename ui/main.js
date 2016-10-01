647dd9f5fab20873bb313c395ac496ffa252ba05
rajatsarkari <rajatsarkari777@gmail.com>
2016-10-01 16:03:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,16 @@ button.onclick = function(){
     request.open('GET', 'http://rajatsarkari.imad.hasura-app.io/counter',true);
     request.send(null);
 }
+var nameInput = document.getElementById('name'); 
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    var names = ['name1','name2','name3'];
+    var list = '';
+    for(var i = 0;i<names.length;i++)
+    {
+        list += '<li>' + names[i] + '</li>';
+    }
+var ul = document.getElementById('namelist');   
+ul.innerHTML = list;
+}
