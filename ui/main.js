b2a907ec97ecb107395316b94ba7feccb59da543
rajatsarkari <rajatsarkari777@gmail.com>
2016-10-01 16:04:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,7 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    var names = ['name1','name2','name3'];
+    var names = ['name1','name2','name3','name4'];
     var list = '';
     for(var i = 0;i<names.length;i++)
     {
