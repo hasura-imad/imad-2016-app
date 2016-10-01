c8976cd313b6356dd4ccc41eb5afcc02310acf38
Mariya <pkmariya@gmail.com>
2016-10-01 01:37:16 +0000

    query parameter

--- server.js
+++ server.js
@@ -111,8 +111,8 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function(req, res) {
-	var name = req.params.name;
+app.get('/submit-name', function(req, res) {
+	var name = req.query.name;
 	names.push(name);
 	res.send(JSON.stringify(names));
 });
