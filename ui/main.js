6fc7fbf8393ee802c69dbc11041a87fd2b7ee57d
anitamahotra63 <anitamahotra63@gmail.com>
2016-10-01 20:35:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,2 @@
 console.log('Loaded!');
  
-var element = document.getElementById('anita');
-var marginleft=0;
-
-function moveRight()
-{
-    marginleft=marginleft+100;
-    element.style.marginleft= marginleft +'px';
-}
-element.onclick=function(){
-   var interval=setInterval(moveRight,100);
-};
