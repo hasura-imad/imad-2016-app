74ba0127251b73207125ae8fcb1b2a05acd3ed55
georgekunchattil1992 <georgekunchattil@gmail.com>
2016-10-02 00:04:49 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -5,18 +5,24 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/',function(req,res){
-  res.sendFile(path.join(_dirname,'ui','index.html'));
-  
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+
 
 
 
