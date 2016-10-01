e12fff05496ba87137abdf32d72a932ee335c825
anitamahotra63 <anitamahotra63@gmail.com>
2016-10-01 16:48:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
  
- var element=getElementById('anita');
+ var element = getElementById('anita');
 
 element.onclick=function(){
-   element.style.marginleft='100px'; 
+   element.style.marginleft = '100px'; 
 };
