ac7598057d4e7258acb04f066388e000934215aa
anitamahotra63 <anitamahotra63@gmail.com>
2016-10-02 14:26:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,27 @@
 var button=document.getElementById("counter");
 
-var counter=0;
+
 button.onclick=function(){
   
+  //create a reqest object
+  var request=new XMLHttpRequest();
+  
+  //capture the response and store it in a variable
+  request.onreadystatechange = function(){
+      if(request.readyState==XMLHttpRequest.DONE)
+      {
+          if(request.status==200)
+          {
+              var counter=request.responseText;
+              var span=document.getElementById('count');
+              span.innerHTML=counter.toString();
+          }
+      }
+  };
   
   
   
   //render the variable in correct span
   counter=counter+1;
-  var span=document.getElementById('count');
-  span.innerHTML=counter;
+  
 };
