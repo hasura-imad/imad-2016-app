bbcb2212cf7a73f859b8b53e8a4f224ddfdf2468
anitamahotra63 <anitamahotra63@gmail.com>
2016-10-02 18:44:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,9 +68,9 @@ app.get('/counter',function(req,res){
 });
 
 var names=[];
-app.get('/submit-name/:name',function(req,res){
+app.get('/submit-name',function(req,res){
    
-   var name=req.params.name;
+   var name=req.query.name;
    names.push(name);
    
    //JSON COMES INTO PLAY:JAVASCRIPT OBJECT NOTATION. which converst the java objects to strings 
