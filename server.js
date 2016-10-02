484acbcf419b50f9508c91a440b82a4d89178314
anitamahotra63 <anitamahotra63@gmail.com>
2016-10-02 12:59:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,6 +61,13 @@ var htmlTemplate=`<html>
 return htmlTemplate;
 }
 
+var counter=0;
+app.get('/counter',function(req,res){
+   counter=counter+1;
+   res.send(counter.toString());
+});
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
