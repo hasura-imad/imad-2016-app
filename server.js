f2abd489c9cc04dc08cd5d872440991fd649e6c6
Mariya <pkmariya@gmail.com>
2016-10-02 03:33:17 +0000

    bug fix

--- server.js
+++ server.js
@@ -90,19 +90,20 @@ var htmlTemplate =
     return htmlTemplate;
 }
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
 var counter = 0;
 app.get('/counter', function(req, res) {
-	counter = counter + 1;
-	res.send(counter.toString());
+        counter = counter + 1;
+        res.send(counter.toString());
 });
+
 app.get('/:articleName', function(req, res) {
 //    res.send(createTemplate(articleOne));
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
