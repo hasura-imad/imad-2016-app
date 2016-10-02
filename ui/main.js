eb75ef9a70a2a6ec327b6c2f1b4a505e0631f183
anitamahotra63 <anitamahotra63@gmail.com>
2016-10-02 10:46:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,13 @@ var element=document.getElementById('main-text');
 element.innerHTML="my name is khan and I am not a terrorist!";
 
 var img=document.getElementById('madi');
+
+function move_to_right()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
+
 img.onclick=function(){
-  img.style.marginLeft='100px';  
+  var interval=setInterval(move_to_right,100); 
 };
