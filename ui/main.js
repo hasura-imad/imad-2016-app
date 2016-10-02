348945d544db906a39be8ff50ee174324eca756c
krusty-crab <jakash1997@gmail.com>
2016-10-02 10:52:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ button.onclick = function () {
    
     request.onreadystatechange = function () {
       if(request.readyState === XMLHttpRequest.DONE){
-             if(request.status === "200"){  //DO NOT COMPARE WITH "200" INSTEAD WITH 200
+             if(request.status === 200){  //DO NOT COMPARE WITH "200" INSTEAD WITH 200
                 var counter=request.responseText; //Should be request.responseText
                 var span = document.getElementById('count');
                 span.innerHTML= counter.toString();
