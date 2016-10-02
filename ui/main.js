a2c5a906b7ff7d343a8899f1b816aa99777476e4
anitamahotra63 <anitamahotra63@gmail.com>
2016-10-02 16:01:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,4 +25,19 @@ button.onclick=function(){
  request.open('GET','http://anitamahotra63.imad.hasura-app.io/counter',true);
  request.send(null);
   
+  var nameInput=document.getElementById('name');
+  var name=nameInput.value;
+  var submit=document.getElementById('sumit_btn');
+  submit.onclick=function()
+  {
+     var names=['name1','name2','name3','name4'];
+     var list='';
+     for(var i=0;i<names.length;i++)
+     {
+         list+='<li>'+names[i]+'</li>';
+     }
+     var ul=document.getElementById('the_list');
+     ul.innerHTML=list;
+     
+  };
 };
