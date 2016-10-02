0a1c574ea712afe11162714280b508cebae82293
anitamahotra63 <anitamahotra63@gmail.com>
2016-10-02 18:19:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,7 @@ button.onclick=function(){
   
   var nameInput=document.getElementById('name');
   var name=nameInput.value;
-  var submit=document.getElementById('sumit_btn');
+  var submit=document.getElementById('submit_btn');
   submit.onclick=function()
   {
      var names=['name1','name2','name3','name4'];
