e3df23df3d4e749446b598bac0fb11ed2b19a1a6
ramit0402 <ramitlegend@gmail.com>
2016-10-03 19:32:29 +0530

    Add files via upload

--- ui/index.php
+++ ui/index.php
@@ -9,17 +9,206 @@ include 'connect.php';
 <title>Online Book Shopping</title>
 </head>
 
+<style>
+
+/* Full-width input fields */
+input[type=text], input[type=password], input[type=email] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+/* Set a style for buttons */
+.a {
+    background-color: transparent;
+    color: white;
+    padding: 14px 20px;
+    margin-left: 350px;
+	margin-top: 20px;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+	text-align:center;
+	border-radius:4px;
+	font-size:20px;
+	font-weight: bold;
+}
+
+.b {
+	background-color:transparent;
+    color: white;
+    padding: 14px 20px;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+	text-align:center;
+	border-radius:4px;
+	font-size:20px;
+}
+
+.a:hover,.b:hover {
+	color:#87bcf8;
+}
+
+
+.c {
+	background-color: #1c478e;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+	text-align:center;
+	border-radius:4px;
+}
+
+.d {
+    background-color: #1c478e;
+    color: white;
+    padding: 14px 15px;
+	margin-top:10px;
+	border: none;
+    cursor: pointer;
+	border-radius:4px;
+    width: 10%;
+	border-radius:4px;
+	text-align:center;
+	position:relative;
+}
+/* Extra styles for the cancel button */
+.cancelbtn {
+    width: 10%;
+	color:white;
+    padding: 10px 18px;
+    background-color: #1c478e;
+	border: none;
+    cursor: pointer;
+    text-align:center;
+	margin: 8px 0;	
+}
+
+/* Center the image and position the close button */
+.imgcontainer {
+    text-align: center;
+    margin: 24px 0 12px 0;
+    position: relative;
+}
+
+img.avatar {
+    width: 40%;
+}
+
+.container {
+    padding: 16px;
+}
+
+span.psw {
+    float: right;
+    padding-top: 16px;
+}
+
+/* The Modal (background) */
+.modal {
+    display: none; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    left: 0;
+    top: 0;
+    width: 80%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+    padding-top: 60px;
+}
+
+/* Modal Content/Box */
+.modal-content {
+    background-color: #fefefe;
+    margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+    border: 1px solid #888;
+    width: 80%; /* Could be more or less, depending on screen size */
+}
+
+/* The Close Button (x) */
+.close {
+    position: absolute;
+    right: 25px;
+    top: 0;
+    color: #000;
+    font-size: 35px;
+    font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+    color: red;
+    cursor: pointer;
+}
+
+/* Add Zoom Animation */
+.animate {
+    -webkit-animation: animatezoom 0.6s;
+    animation: animatezoom 0.6s
+}
+
+@-webkit-keyframes animatezoom {
+    from {-webkit-transform: scale(0)}
+    to {-webkit-transform: scale(1)}
+}
+    
+@keyframes animatezoom {
+    from {transform: scale(0)}
+    to {transform: scale(1)}
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+    span.psw {
+       display: block;
+       float: none;
+    }
+    .cancelbtn {
+       width: 100%;
+    }
+}
+
+input[name=text1] {
+	width: 130px;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px;
+    background-color: white;
+    background-image: url('search.png');
+    background-position: -3px -3px;
+    background-repeat: no-repeat;
+    padding: 12px 20px 12px 50px;
+    -webkit-transition: width 0.4s ease-in-out;
+    transition: width 0.4s ease-in-out;
+}
+
+input[name=text1]:focus {
+width: 70%;}
+	
+</style>
+
 <body>
 <div id="container">
 	<div id="header">
 	<img width="8%" src="images/logo.png" alt="web developer directory">
-        <h1><span class="blue-text">Bay</span> OfBooks</h1>
-        <h2><a href="login.html"><span class="bl-text">Login</span></a> | <a href="signup.php"><span class="white-text">Sign Up</span></a></h2>
+        <h1>Bay Of Books</h1>
+        <button class="a" onclick="document.getElementById('id02').style.display='block'" style="width:auto;">Login</button>
+		<button class="b" onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Register</button>
      </div><!--header end-->
 	 
 	 <ul class="menu">
 		<li class="dropdown">
-			<a href="index.html" class="dropbtn">Home</a>
+			<a href="index.php" class="dropbtn">Home</a>
 		</li>
 		<li class="dropdown">
 			<a href="#" class="dropbtn">Authors</a>
@@ -84,10 +273,128 @@ include 'connect.php';
         </ul>
     </div><!--leftmenu end-->
     
-	<div id="content">
+	<div id="content" style="float:left;">
 		<!--CONTENT HERE-->
+		<select style="display:inline;float:left;position:relative;height:45px;margin-right:10px;margin-top:9px;padding:10px;background-color:#1c478e;color:white;border:0;border-radius:5px">
+			<option value="CBD">CBD</option>
+			<option value="Nerul">Nerul</option>
+			<option value="Kharghar">Kharghar</option>
+			<option value="Vashi">Vashi</option>
+		</select>
+		<form action="page1.php" method="post">
+			<input type="text" name="text1" name="search" placeholder="Search.." style="display:inline;float:left;margin-right:10px">
+		</form>
+		<input type="submit" class="d" value="Search"/ style="display:inline;float:left;margin-top:9px;height:45px;">
+			
     </div><!--content end-->
 	
 </div><!--container end-->
+
+
+<div id="id01" class="modal">
+  
+  <form class="modal-content animate" action="register_insert.php" method="post">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+      <img src="avatar2.jpg" alt="Avatar" class="avatar">
+    </div>
+
+    <div class="container">
+      <label><b>Name</b></label>
+      <input type="text" placeholder="Enter Name" name="name" required>
+
+      <label><b>Username</b></label>
+      <input type="text" placeholder="Enter Userame" name="username" required>
+	 
+      <label><b>Email</b></label>
+      <input type="email" placeholder="Enter Email" name="email" required>
+      
+	  <label><b>Password</b></label>
+	  <input type="password" placeholder="Enter Password" name="pwd" required>	   
+      
+	  <label><b>Phone number</b></label>
+      <input type="text" placeholder="Enter Phone number" name="phone" required>	   
+      
+	  <label><b>Address Line 1</b></label>
+      <input type="text" placeholder="Enter Address Line 1" name="add1" required>	   
+      
+	  <label><b>Address Line 2</b></label>
+      <input type="text" placeholder="Enter Address Line 2" name="add2">	   
+      
+      <label><b>City</b></label>
+      <input type="text" placeholder="Enter City" name="city" required>
+
+      <label><b>State</b></label>
+      <input type="text" placeholder="Enter State" name="state" required>
+
+      <label><b>Pin Code</b></label>
+      <input type="text" placeholder="Enter Pin Code" name="pin" required>
+
+	  <button class="c" type="submit">Register</button>
+    </div>
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+    </div>
+    
+  </form>
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+</script>
+
+<div id="id02" class="modal">
+  
+  <form class="modal-content animate" action="authenticate.php" method="post">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id02').style.display='none'" class="close" title="Close Modal">&times;</span>
+      <img src="avatar2.jpg" alt="Avatar" class="avatar">
+    </div>
+
+    <div class="container">
+      <label><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="username" required>
+
+      <label><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="password" required>
+        
+      <button class="c" type="submit">Login</button>
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelbtn">Cancel</button>
+    </div>
+  </form>
+</div>
+
+<div id="id03" class="modal">
+  
+  <form class="modal-content animate" action="authenticate.php" method="post">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id02').style.display='none'" class="close" title="Close Modal">&times;</span>
+      </div>
+
+    <div class="container">
+      <label><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="username" required>
+
+      <label><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="password" required>
+        
+      <button class="c" type="submit">Login</button>
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelbtn">Cancel</button>
+    </div>
+  </form>
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById('id02');
+</script>
+
 </body>
 </html>
