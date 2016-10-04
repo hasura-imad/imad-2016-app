aa49d09096c595220f3e17d6f336b4b93f4ea695
luckyman000786 <luckyman000786@gmail.com>
2016-10-04 17:11:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,11 +55,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter =0;
+app.get('/counter',function(req,res){
+counter = counter+1;
+res.send(counter.toString());
+});
     
-//get('/:articleName', function (req, res) {
-  //  var articleName = req.params.articleName;
-  //res.send(createTemplate(articles[articleName]));
-//});
+app.get('/:articleName', function (req, res) {
+   var articleName = req.params.articleName;
+ res.send(createTemplate(articles[articleName]));});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -71,16 +75,13 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
 app.get('/submit-btn',function(req,res){
 var name = req.query.name;
 names.push(name);
 req.send(JSON.stringify(names));
 });
-var counter =0;
-app.get('/counter',function(req,res){
-counter = counter+1;
-res.send(counter.toString());
-});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
