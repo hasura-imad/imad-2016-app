739456790ff31232c8bfa3af3155390d24e21a7e
luckyman000786 <luckyman000786@gmail.com>
2016-10-04 17:08:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,12 +50,12 @@ var htmlTemplate=`<html>
 </div>
 </body>
 </html>`; return htmlTemplate;}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
     
-});
 //get('/:articleName', function (req, res) {
   //  var articleName = req.params.articleName;
   //res.send(createTemplate(articles[articleName]));
@@ -80,7 +80,7 @@ var counter =0;
 app.get('/counter',function(req,res){
 counter = counter+1;
 res.send(counter.toString());
-
+});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
