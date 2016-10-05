dbe48ab6ee2e9b83110fa1627b4f44d1c85a2743
krusty-crab <jakash1997@gmail.com>
2016-10-05 10:21:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,13 +26,27 @@ var name=nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     //make request to get names
-    
+     var request = new XMLHttpRequest(); 
+   
+    request.onreadystatechange = function () {
+      if(request.readyState === XMLHttpRequest.DONE){
+             if(request.status === 200){  
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list='';
+                for(var i=0; i<names.length;i++){
+                    list += '<li>' + names[i] + '</li>';
+                    var ul=document.getElementById('namelist');
+                    ul.innerHTML = list;
+                }
+            }
+        }
+    };
+
+    request.open("GET", document.URL+"submit-name?name="+name, true);
+    request.send(null);
     // capture names
-    var names = ['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0; i<names.length;i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML = list;
+   
+    
+    
 };
