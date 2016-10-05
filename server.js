0011d809f792cf3c849f1c7bd3d8043114c6566f
krusty-crab <jakash1997@gmail.com>
2016-10-05 10:10:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/', function (req, res) {
 });
 
 var names=[];
-app.get('/submit-name/:name', function(req, res) {
+app.get('/submit-name', function(req, res) {
    var name = req.query.name;
    names.push(name);
    res.send(JSON.stringify(names));
