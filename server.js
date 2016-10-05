0c2779ac57f025430a9f2413c6935f3385b488c7
ramit0402 <ramitlegend@gmail.com>
2016-10-05 23:10:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.php'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/signup.html', function (req, res) {
