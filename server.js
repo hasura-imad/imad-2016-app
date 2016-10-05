1d13f400feb9493cbdcad022ca65875933e3730d
krusty-crab <jakash1997@gmail.com>
2016-10-05 09:57:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 var names=[];
 app.get('/submit-name:name', function(req, res) {
-   var name;
+   var name = req.params.name;
    names.push(name);
    res.send(JSON.stringify(names));
 });
