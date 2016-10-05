54a560d68d6905d576a91c9b2e3d6e8610d2d345
krusty-crab <jakash1997@gmail.com>
2016-10-05 09:01:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,24 @@ button.onclick = function () {
         }
     };
 
-  request.open("GET", document.URL+"counter", true);
+    request.open("GET", document.URL+"counter", true);
     request.send(null);
+};
 
+// Get Name
+
+var nameInput = document.getElementById('name');
+var name=nameInput.value;
+var submit = document.getElementById('sumbit_btn');
+submit.onclick = function () {
+    //make request to get names
+    
+    // capture names
+    var names = ['name1','name2','name3'];
+    var list='';
+    for(var i=0; i<names.length;i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML = list;
 };
