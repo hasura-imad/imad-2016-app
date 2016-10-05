f609c4366a69a54d89cf264784ac4be72cfc7166
krusty-crab <jakash1997@gmail.com>
2016-10-05 09:02:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ submit.onclick = function () {
     //make request to get names
     
     // capture names
-    var names = ['name1','name2','name3'];
+    var names = ['name1','name2','name3','name4'];
     var list='';
     for(var i=0; i<names.length;i++){
         list += '<li>' + names[i] + '</li>';
