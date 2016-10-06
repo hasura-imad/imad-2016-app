9f082187ee52959a81c8bfd2dcbdd9bd174ceb01
Mariya <pkmariya@gmail.com>
2016-10-06 14:55:26 +0000

    url update

--- ui/main.js
+++ ui/main.js
@@ -57,6 +57,6 @@ submit.onclick = function (){
 	};
 
 	var nameInput = document.getElementById('name').value;	
-	request.open('GET', 'http://pkmariya.imad.hasura-app.io/submit-name?name=', true);
+	request.open('GET', 'http://pkmariya.imad.hasura-app.io/submit-name?name=' + name, true);
 	request.send(null);
 };
