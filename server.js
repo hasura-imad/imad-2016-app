743ee85392c2c4f91add21378c86ba1b6e1f5b47
golua94 <kumar.vikash863@gmail.com>
2016-10-06 12:08:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/Viku1', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Viku1.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
