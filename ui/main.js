83d92c19a36ff9cc09e6ef096f008cd6852dc8b8
golua94 <kumar.vikash863@gmail.com>
2016-10-06 19:37:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,13 @@ var element=document.getElementById('main-text');
 
 
 element.innerHTML='New doc';
+//moving
+
+var img=document.getElementById('main-img');
+
+
+img.onclick=function(){
+    
+    
+    img.style.marginleft="100px";
+}
