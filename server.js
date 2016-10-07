a69a6ae83fa9e58519f01e460960a2542ef8d8eb
golua94 <kumar.vikash863@gmail.com>
2016-10-07 11:31:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,6 +90,11 @@ return htmltemplate;}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter =0;
+app.get('/counter', function (req, res) {
+   counter=counter +1;
+  res.send(counter.toString());
+});
 
 app.get('/:Vikuname', function (req, res) {
   var Vikuname=req.params.Vikuname;
