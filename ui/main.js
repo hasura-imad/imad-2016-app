0eae06315be27f7e240ec63dc96824db3b6a3c27
golua94 <kumar.vikash863@gmail.com>
2016-10-07 15:54:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,23 @@ button.onclick = function(){
    request.send(null);
    
 };
+
+var nameInput=document.getElementById('name');
+
+var name =nameInput.value;
+
+var submit=document.getElementById('submit_btn');
+
+submit.onclick = function(){
+    
+    
+    var names= ['name1', 'name2', 'name3', 'name4'];
+    var list='';
+    
+    for(var i=0; i<names.length; i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+};
