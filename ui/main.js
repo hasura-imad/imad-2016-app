6bc28c2db21132751d4a9a39c3a8bee6c56e74b4
golua94 <kumar.vikash863@gmail.com>
2016-10-07 14:35:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ button.onclick = function(){
                 var counter= request.responseText;
                 
                 var span=document.getElementById('count');
-                 span.innerHTML=counter.toString();
+                 span.innerHTML=counter;
             }
         }
     };
