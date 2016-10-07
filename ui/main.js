ded5135d3f729a121e29cad6c487a6b69df7c4b6
golua94 <kumar.vikash863@gmail.com>
2016-10-07 14:30:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,4 @@
-/*console.log('Loaded!');
-var element=document.getElementById('main-text');
 
-
-element.innerHTML='New doc';
-//moving
-
-var img=document.getElementById('main-img');
-var marginLeft=0;
-function moveright(){
-    marginLeft= marginLeft + 1;
-img.style.marginLeft= marginLeft + 'px';
-}
-
-img.onclick=function(){
-    var interval=setInterval(moveright, 10);
-    
-    
-}*/
 
 var button=document.getElementById('counter');
 
