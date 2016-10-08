7b213fc5785836815dfa92f72dbcd056ccb27e07
Mariya <pkmariya@gmail.com>
2016-10-08 13:35:32 +0000

    test changes

--- ui/main.js
+++ ui/main.js
@@ -18,7 +18,10 @@ img.onclick = function ()
 
 var button = document.getElementById('counter');
 
+var counter = 0;
 button.onclick = function() {
+
+/*
 	var request = new XMLHttpRequest();
 
 	request.onreadystatechange = function() {
@@ -33,6 +36,12 @@ button.onclick = function() {
 	
 	request.open('GET', 'http://pkmariya.imad.hasura-app.io/counter', true);
 	request.send(null);
+*/
+
+counter = counter + 1;
+var span = document.getElementById('count');
+span.innerHTML = counter.toString();
+
 };
 
 var submit = document.getElementById('submit_btn');
