741e9612497a7fc0f4d89ff94e1b6f8a13119bf1
anupambiswas059 <anupambiswas059@gmail.com>
2016-10-10 03:05:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,9 +15,8 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/abc.html',function(req,res) {
-    
-    res.sendFile(path.join(__dirname, 'ui', 'abc.html'));
+app.get('/ui/abc.html',function(req,res) {
+       res.sendFile(path.join(__dirname, 'ui', 'abc.html'));
 });
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
