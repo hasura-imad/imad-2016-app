344b1609fb0da3801f69a0bea3131c5c966274be
anupambiswas059 <anupambiswas059@gmail.com>
2016-10-10 04:17:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter+=1;
+    res.send(counter.tostring());
+    
+});
 app.get('/abc.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'abc.html'));
 });
