fe2fd3a08188a5b34deebe14a17fd39ac24111ea
suryan123 <suryanprasadom@gmail.com>
2016-10-18 19:07:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,3 +28,6 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+
+
+alert("The paragraph is now hidden");
