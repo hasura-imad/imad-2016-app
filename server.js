459249897b6aa7ad474650996cade47fcb205390
krusty-crab <jakash1997@gmail.com>
2016-10-19 22:06:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+/*var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+  console.log(`IMAD course app listening on port ${port}!`);*/
 });
