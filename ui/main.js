b1f5837bbf536ead5c2d168d771ea4d7b9c0f57c
Bhanu <Bhanugyani@gmail.com>
2016-10-21 21:09:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,6 @@ var imgo =document.getElementsByClassName("img-medium");
 var marginleft = 5;
 function moveleft(){
     marginleft = marginleft + 3;
-    imgo.style.marginLeft="marginleft";
+    imgo.style.marginLeft="marginleft+'px'";
 }
 imgo.addEventListener("click", moveleft);
