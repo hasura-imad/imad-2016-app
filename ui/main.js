6f5be2db9d220a17da99988e07a28dada09df8b1
Bhanu <Bhanugyani@gmail.com>
2016-10-22 01:10:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,11 @@
 console.log('Loaded!');
 
 var imgo =document.getElementById("madi");
-
+var marginleft=0;
+var h=0;
 function moveleft(){
-    imgo.style.marginLeft="90px";
+    marginleft= marginleft + 10 ;
+    h=marginleft + "px";
+    imgo.style.marginLeft="h";
 }
 imgo.addEventListener("click", moveleft);
