bdf64fe59e2840202308a98cea491317fdfab32c
Bhanu <Bhanugyani@gmail.com>
2016-10-21 20:54:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,5 +5,5 @@ var marginleft = 5;
 var moveleft=function(){
     marginleft = marginleft + 3;
     img.style.marginLeft=marginleft+"px";
-}
-img.addEventListener("click",moveleft);
+};
+img.addEventListener("click", moveleft);
