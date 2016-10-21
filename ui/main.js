7f4f70e3e7a81bba4144d3b8c08da89b49881ebb
Bhanu <Bhanugyani@gmail.com>
2016-10-22 01:18:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var imgo =document.getElementById("madi");
 var marginleft=0;
 var h=0;
-var interval = setInterval(moveleft,1000);
+var interval = setInterval(moveleft,10);
 function moveleft(){
     marginleft= marginleft + 10 ;
    
