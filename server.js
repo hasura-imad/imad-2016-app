920225ed7c68d6e2deb641976f02af75219d9266
Coco <tanmaig@hasura.io>
2016-10-23 03:24:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ app.get('/articles/:articleName', function (req, res) {
   // articleName == article-one
   // articles[articleName] == {} content object for article one
   
-  // SELECT * FROM article WHERE title = 'article-one'
+  // SELECT * FROM article WHERE title = ''; DELETE WHERE a = 'asdf'
   pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err, result) {
     if (err) {
         res.status(500).send(err.toString());
