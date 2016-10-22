dc09cb094cbcdafc4554e1a49374e9b634adcc3a
Coco <tanmaig@hasura.io>
2016-10-23 03:04:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,11 +121,22 @@ app.get('/submit-name', function(req, res) { // /submit-name?name=xxxx
   res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
   // articleName == article-one
   // articles[articleName] == {} content object for article one
-  var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+  
+  pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function (err, result) {
+    if (err) {
+        res.status(500).send(err.toString());
+    } else {
+        if (result.rows.length === 0) {
+            res.status(404).send('Article not found');
+        } else {
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+        }
+    }
+  });
 });
 
 app.get('/ui/style.css', function (req, res) {
