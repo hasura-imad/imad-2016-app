b369c59ab6d48d45456f2d75f6e6f67db5ca8826
Coco <tanmaig@hasura.io>
2016-10-23 02:49:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/test-db', function (req, res) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
-          res.send(JSON.stringify(result));
+          res.send(JSON.stringify(result.rows));
       }
    });
 });
