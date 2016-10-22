b16f2af58cefdffb431a1dc864d19cec9500e730
Coco <tanmaig@hasura.io>
2016-10-23 03:07:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ function createTemplate (data) {
                   ${heading}
               </h3>
               <div>
-                  ${date}
+                  ${date.toDateString()}
               </div>
               <div>
                 ${content}
