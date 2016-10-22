e85d9e0cdc6fd583d07a4675ed63cd2e9309b72c
Coco <tanmaig@hasura.io>
2016-10-23 03:06:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,7 +125,8 @@ app.get('/articles/:articleName', function (req, res) {
   // articleName == article-one
   // articles[articleName] == {} content object for article one
   
-  pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function (err, result) {
+  // SELECT * FROM article WHERE title = 'article-one'
+  pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err, result) {
     if (err) {
         res.status(500).send(err.toString());
     } else {
