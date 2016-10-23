369e98c8362af43ff53ba436840d0ae3a508d0ac
Bhanu <Bhanugyani@gmail.com>
2016-10-23 23:48:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -32,6 +32,9 @@ app.get('/newcon.php', function (req, res) {
 app.get('/corct.php', function (req, res) {
   res.sendFile(path.join(__dirname, 'corct.php'));
 });
+app.get('/corct1.php', function (req, res) {
+  res.sendFile(path.join(__dirname, 'corct1.php'));
+});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
