67c22c2c72b20f0300422706c87005568d5d4089
Bhanu <Bhanugyani@gmail.com>
2016-10-23 23:44:18 +0530

    Add files via upload

--- corct.php
+++ corct.php
@@ -10,27 +10,27 @@
 <?php
 include('connection.php');
 ?>
-<form metod ="post" action="new.php">>
+<form metod ="post" action="corct1.php">>
 <h1>form<hr></h1>
 <table>
 <tr>
 <td>NAME</td>
-<td><input type="text"></td>
+<td><input type="text"   name="name"></td>
 </tr>
 <tr>
   <td>BRANCH</td>
-   <td><input type="text" name="fname" ></td>
+   <td><input type="text" name="branch" ></td>
 </tr>
  <tr>
   <td>EMAIL</td>
-  <td><input text="email"></td>
+  <td><input text="email" name="email"></td>
 <tr>
    <td>RATE US</td>
-   <td><input type="number" min="0" max="10"></td>
+   <td><input type="number" min="0" max="10" name="number"></td>
    </tr>
    <tr>
    <td>DATE OF BIRTH</td>
-   <td><input  type="DATE" min="2015-01-01" max="2016-01-02" >
+   <td><input  type="DATE" min="2015-01-01" max="2016-01-02"  name="date">
  <tr>
    <td></td>
     <td><input type="submit" ></td>
