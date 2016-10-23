d5a67422fd19cfed689a2a460ad70e8cf8ad61f6
Mariya <pkmariya@gmail.com>
2016-10-23 21:48:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.get('/:articleName', function(req, res) {
 });
 
 
-var port = 8081; // Use 8080 for local development because you might already have apache running on 80
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
