3ec77c4835c508e3df8c3755d902d1f5eb025698
Mariya <pkmariya@gmail.com>
2016-10-23 22:12:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+  user: 'pkmariya', 
+  database: 'pkmariya',
+  host: 'db.imad.hasura-app.io',
+  port: '5432',
+  password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -98,6 +107,21 @@ app.get('/counter', function(req, res) {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req, res) 
+{
+   pool.query('SELECT * FROM test', function(err, result)
+   {
+      if(err)
+      {
+          res.status(500).send(err.toString());
+      } else
+      {
+          res.send(JSON.stringfy(result));
+      }
+   });
 });
 
 app.get('/ui/style.css', function (req, res) {
