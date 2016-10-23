38c5895798558a9c84f8c12c3e6fc72f2a1b656b
Mariya <pkmariya@gmail.com>
2016-10-23 22:19:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.get('/test-db', function(req, res)
           res.status(500).send(err.toString());
       } else
       {
-          res.send(JSON.stringify(result));
+          res.send(JSON.stringify(result.rows));
       }
    });
 });
