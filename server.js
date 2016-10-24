bf2138258d4982cbfed073c7cb3c589764168820
akshatbhargava123 <akshatbhargavadeepa@gmail.com>
2016-10-24 23:52:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.get('/test-db', function(req, res){
             res.status(500).send(err.toString());
         }
         else{
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.rows));
         }
     })
 })
