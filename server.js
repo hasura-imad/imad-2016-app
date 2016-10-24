7421cb522e0ae86109ab3e6698ef699254db9ca9
akshatbhargava123 <akshatbhargavadeepa@gmail.com>
2016-10-24 23:52:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var Pool = require('pg').Pool
 
 var config = {
     user: 'akshatbhargava123',
-    database: 'akshatbhargav123',
+    database: 'akshatbhargava123',
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD // environment variable
