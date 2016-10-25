1a384bd4946e51081dcffd48907335995d3c5c29
luckyman000786 <luckyman000786@gmail.com>
2016-10-25 16:39:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,11 @@ res.send(counter.toString());
     
 app.get('/:articleName', function (req, res) {
    var articleName = req.params.articleName;
- res.send(createTemplate(articles[articleName]));});
+ res.send(createTemplate(articles[articleName]));})
+
+app.get('/login.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'login.html'));
+}); 
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
