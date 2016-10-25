84e04824d4f5d48dd152dd6ffc5ac1a8c4b949a7
achyut <a0134598@u.nus.edu>
2016-10-25 12:11:22 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     database: 'achyut92',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: 'process.env.DB_PASSWORD'
+    password: process.env.DB_PASSWORD
 };
 
 var app = express();
