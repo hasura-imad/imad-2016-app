48de97f8f20ab1ae7f49067a37be49a9e2a24d00
achyut <a0134598@u.nus.edu>
2016-10-25 12:27:30 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get('/articles/:articleName', function (req, res) {
 		if(err){
            res.status(500).send(err.toString());
        }else{
-           if(result.rows.length == 0){
+           if(result.rows.length === 0){
            	res.status(404).send('Article Not Found.');
            }else{
            	res.send(createTemplate(result.rows[0]));
