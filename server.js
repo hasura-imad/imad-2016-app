ec70da6bf12f3e4a231333b725bf33c0ca1acea0
Bhanu <Bhanugyani@gmail.com>
2016-10-25 18:14:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -32,6 +32,9 @@ app.get('/newcon1.php', function (req, res) {
 app.get('/corct.php', function (req, res) {
   res.sendFile(path.join(__dirname, 'corct.php'));
 });
+app.get('/copytst.php', function (req, res) {
+  res.sendFile(path.join(__dirname, 'copytst.php'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
