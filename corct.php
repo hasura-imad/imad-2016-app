abf64e3cec8f6802ea2b3e9f4d0f659be5c03b9e
Bhanu <Bhanugyani@gmail.com>
2016-10-25 17:28:40 +0530

    Add files via upload

--- corct.php
+++ corct.php
@@ -11,7 +11,7 @@
 include'newcon1.php';
 $dbconnection = new dbconnection();
 ?>
-<form  metod ="post" action="corct.php">>
+<form  metod ="post" action=""<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);">>
 <h1>form<hr></h1>
 <table>
 <tr>
@@ -44,4 +44,93 @@ Your branch <?php echo $_POST['branch']; ?>
 email <?php echo $_POST['email'];?>
 dob <?php echo $_POST['date'];?>
 </body></div>
+
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+  }
+    
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+  }
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field.</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
+<?php
+  $dbconn = pg_connect("dbname=publisher") or die("Could not connect");
+  $stat = pg_connection_status($dbconn);
+  if ($stat === PGSQL_CONNECTION_OK) {
+      echo 'Connection status ok';
+  } else {
+      echo 'Connection status bad';
+  }    
+?>
 </html>
