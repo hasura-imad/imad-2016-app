6b1195d79a9fe640ddb1ca682e2450f0e528b073
krlalliram <krlalliram@gmail.com>
2016-10-27 16:31:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/tpty.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'tpty.jpg'));
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
