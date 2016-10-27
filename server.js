43d495e5b222cd317a1afb690d7de75e66a49c26
krlalliram <krlalliram@gmail.com>
2016-10-27 16:43:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
@@ -22,13 +23,13 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+*/
 
 
 
 
 
-
-/* different program
+/* different program */
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
@@ -119,4 +120,3 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
-*/
