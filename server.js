a53ada856449fcefb54cdf2eb34dae70ac9b0617
luckyman000786 <luckyman000786@gmail.com>
2016-10-27 13:37:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var config = {
+user : 'luckyman000786',database :'luckyman000786',host:'db.imad.hasura-app.io',  port : '55432',password : process.env.DB_PASSWORD
+    };
+var Pool = require('pg').Pool;
 var app = express();
 app.use(morgan('combined'));
 var names = [];
+
 
 
 var articles = {
@@ -55,6 +59,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
+
+app.get('test-db',function(req,res){
+    //make a select request
+    //return a response with the results
+   pool.query('SELECT * FROM user',function(err,result){
+       if(err){res.status(500).send(err.toString());}else{res.send(JSON.stringify(result));}
+   }); 
+     
+});
 var counter =0;
 app.get('/counter',function(req,res){
 counter = counter+1;
