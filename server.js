151d9491a56abdb814e819911ce05e6f58c6c093
luckyman000786 <luckyman000786@gmail.com>
2016-10-27 16:31:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -52,8 +52,8 @@ res.status(400).send("Article Not Found!! Retry with Valid Article ID");
     
     
 });
-   var articleName = req.params.id;
- })
+  
+ });
 
 app.get('/login.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'login.html'));
@@ -75,11 +75,11 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/submit-btn',function(req,res){
-var name = req.query.name;
-names.push(name);
-req.send(JSON.stringify(names));
-});
+//app.get('/submit-btn',function(req,res){
+//var name = req.query.name;
+//names.push(name);
+//req.send(JSON.stringify(names));
+//});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
