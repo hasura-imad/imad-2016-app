fad96b361e75bb947088ae466049517477e2e6c6
ramit0402 <ramitlegend@gmail.com>
2016-10-29 14:15:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,20 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/signup.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'signup.html'));
+app.get('/css/bootstrap.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'css', 'bootstrap.css'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/css/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'css', 'style.css'));
 });
 
-app.get('/ui/styles.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'styles.css'));
-});
-
-app.get('/ui/logo.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'logo.png'));
+app.get('/images/logo.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'images', 'logo.png'));
 });
 
 app.get('/ui/background.png', function (req, res) {
