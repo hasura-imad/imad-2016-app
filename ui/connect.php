065744bf8f21d679950b6edcb6cad036c9369e81
ramit0402 <ramitlegend@gmail.com>
2016-10-29 12:48:47 +0530

    Delete connect.php

--- ui/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$hostname = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'bayofbooks';
-$connect = mysqli_connect($hostname,$username,$password,$dbname) or die("Error Connecting");
-?>
