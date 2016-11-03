5114886be8cbac852ea1865ce289e436c864b934
Coco <tanmaig@hasura.io>
2016-11-04 00:23:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,31 @@ app.post('/create-user', function (req, res) {
    });
 });
 
+app.post('/login', function (req, res) {
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   pool.query('SELECT * from "user" username = $1', [username], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          if (result.rows.length === 0) {
+              res.send(403).send('username/password is invalid');
+          } else {
+              // Match the password
+              var dbString = result.rows[0].password;
+              var salt = dbString.split('$')[2];
+              var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
+              if (hashedPassword === dbString) {
+                res.send('credentials correct!');
+              } else {
+                res.send(403).send('username/password is invalid');
+              }
+          }
+      }
+   });
+});
+
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
    // make a select request
