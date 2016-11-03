09d33f530771e85040a5ec929faf4a1e03a50fa8
Coco <tanmaig@hasura.io>
2016-11-04 00:25:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.post('/login', function (req, res) {
    var username = req.body.username;
    var password = req.body.password;
    
-   pool.query('SELECT * from "user" username = $1', [username], function (err, result) {
+   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
