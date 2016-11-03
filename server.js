a98a4e26935a2625f176afbe2359454d8c4d018b
Coco <tanmaig@hasura.io>
2016-11-04 02:07:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,7 +125,13 @@ app.post('/login', function (req, res) {
    });
 });
 
-
+app.get('/check-login', function (req, res) {
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       res.send('You are logged in: ' + req.session.auth.userId.toString());
+   } else {
+       res.send('You are not logged in');
+   }
+});
 
 
 var pool = new Pool(config);
