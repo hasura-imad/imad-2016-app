f81465fd2f5f45617b476fd723e09cebd2776aec
Coco <tanmaig@hasura.io>
2016-11-04 01:43:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,6 +27,7 @@ submit.onclick = function () {
     console.log(username);
     console.log(password);
     request.open('POST', 'http://coco98.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username: username, password: password}));  
 
 };
