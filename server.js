8d6bec1a3b6dd3db5e784dfb41a0829e67f861a1
Coco <tanmaig@hasura.io>
2016-11-04 04:03:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -213,17 +213,10 @@ app.get('/articles/:articleName', function (req, res) {
   });
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/ui/:fileName', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
 
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
