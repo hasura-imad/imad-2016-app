4b07e90ac38b3a72e51a1d8f14a678c8c8631483
Coco <tanmaig@hasura.io>
2016-11-04 01:41:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,5 @@
-// Counter code
-var button = document.getElementById('counter');
 
-button.onclick = function () {
-
-    // Create a request object
-    var request = new XMLHttpRequest();
-    
-    // Capture the response and store it in a variable
-    request.onreadystatechange = function () {
-      if (request.readyState === XMLHttpRequest.DONE) {
-          // Take some action
-          if (request.status === 200) {
-              var counter = request.responseText;
-              var span = document.getElementById('count');
-              span.innerHTML = counter.toString();          
-          }
-      }  
-      // Not done yet
-    };
-    
-    // Make the request
-    request.open('GET', 'http://coco98.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
-
-// Submit name
+// Submit username/password to login
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
   
@@ -36,24 +11,22 @@ submit.onclick = function () {
       if (request.readyState === XMLHttpRequest.DONE) {
           // Take some action
           if (request.status === 200) {
-              // Capture a list of names and render it as a list
-              var names = request.responseText;
-              names = JSON.parse(names);
-              var list = '';
-              for (var i=0; i< names.length; i++) {
-                  list += '<li>' + names[i] + '</li>';
-              }
-              var ul = document.getElementById('namelist');
-              ul.innerHTML = list;   
+              alert('Logged in successfully');
+          } else if (request.status === 403) {
+              alert('Username/password is incorrect');
+          } else if (request.status === 500) {
+              alert('Something went wrong on the server');
           }
       }  
       // Not done yet
     };
     
     // Make the request
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET', 'http://coco98.imad.hasura-app.io/submit-name?name=' + name, true);
-    request.send(null);  
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://coco98.imad.hasura-app.io/login', true);
+    request.send(JSON.strinfigy({username: username, password: password}));  
 
 };
