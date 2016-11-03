727f87ee53a1897b178b64041539f36496da84e3
Coco <tanmaig@hasura.io>
2016-11-04 04:10:31 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -4,7 +4,7 @@ var currentArticleTitle = window.location.pathname.split('/')[2];
 function loadCommentForm () {
     var commentFormHtml = `
         <h5>Submit a comment</h5>
-        <textarea id="comment_text" placeholder="Enter your comment here...">
+        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here...">
         </textarea>
         <input type="submit" id="submit" value="Submit" />
         <br/>
