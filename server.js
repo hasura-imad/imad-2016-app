3be99db2b738f55aadbf93dffa79401789a98540
Coco <tanmaig@hasura.io>
2016-11-04 03:07:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,9 +128,16 @@ app.post('/login', function (req, res) {
 
 app.get('/check-login', function (req, res) {
    if (req.session && req.session.auth && req.session.auth.userId) {
-       res.send('You are logged in: ' + req.session.auth.userId.toString());
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
    } else {
-       res.send('You are not logged in');
+       res.status(404).send('You are not logged in');
    }
 });
 
