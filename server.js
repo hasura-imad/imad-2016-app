02086de0e8c0e4b7be6240bf06b8cbf439c4e447
ramit0402 <ramitlegend@gmail.com>
2016-11-03 13:53:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var Pool = require('pg').Pool;
 var config ={
     user: 'ramit0402',
     database: 'ramit0402',
-    hist: 'db.imad.hasura-app.io',
+    host: 'db.imad.hasura-app.io',
     port:'5432',
     password: process.env.DB_PASSWORD
 };
