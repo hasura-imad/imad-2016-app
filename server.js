665b2b82311b15f11cc38e07cca0cf6703c0e89a
ramit0402 <ramitlegend@gmail.com>
2016-11-03 08:24:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,19 +2,6 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-var http = require("http");
-http.createServer(function (request, response) {
- // Send the HTTP header
- // HTTP Status: 200 : OK
- // Content Type: text/plain
- response.writeHead(200, {'Content-Type': 'text/plain'});
-
- // Send the response body as "Hello World"
- response.end('Hello World\n');
-}).listen(5432);
-// Console will print the message
-console.log('Server running at http://http://ramit0402.imad.hasura-app.io/');
-
 
 var app = express();
 app.use(morgan('combined'));
