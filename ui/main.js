645886efe5fea8f322efd39e51e6659e3e588898
Coco <tanmaig@hasura.io>
2016-11-04 03:23:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -106,5 +106,33 @@ function loadLogin () {
     request.send(null);
 }
 
+function loadArticles () {
+        // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var articles = document.getElementById('articles');
+            if (request.status === 200) {
+                var content = '<ul>';
+                var articleData = JSON.parse(this.responseText);
+                for (var i=0; i< articleData.length; i++) {
+                    content += `<li><a href="/articles/${articleData[i].title}">${articleData[i].heading}</a></li>`;
+                }
+                content += "</ul>"
+                articles.innerHTML = content;
+            } else {
+                articles.innerHTML('Oops! Could not load all articles!')
+            }
+        }
+    };
+    
+    request.open('GET', '/get-articles', true);
+    request.send(null);
+}
+
+
 // The first thing to do is to check if the user is logged in!
 loadLogin();
+
+// Now this is something that we could have directly done on the server-side using templating too!
+loadArticles();
