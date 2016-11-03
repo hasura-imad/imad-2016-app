a0be79e223d472a61afa9c0d5fd056461f342a69
ramit0402 <ramitlegend@gmail.com>
2016-11-03 08:47:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config ={
+    user: 'ramit0402',
+    database: 'ramit0402',
+    hist: 'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+};
 
 
 var app = express();
