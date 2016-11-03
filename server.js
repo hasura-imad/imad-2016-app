cf6148e060628f4edafd12c185eba7d8a70f4dfe
ramit0402 <ramitlegend@gmail.com>
2016-11-03 09:27:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,16 +15,20 @@ var config ={
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
+var pool =new Pool(config);
+app.get('/test-db', function (req, res) {
+    
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
 
 app.get('/index.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
