f89207b1482cee8f4ba8e20c20749e4cca0dd245
Coco <tanmaig@hasura.io>
2016-11-04 04:35:39 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -59,7 +59,7 @@ function loadLogin () {
 
 function escapeHTML (text)
 {
-    var $text = document.createTextNode(html);
+    var $text = document.createTextNode(text);
     var $div = document.createElement('div');
     $div.appendChild($text);
     return $div.innerHTML;
