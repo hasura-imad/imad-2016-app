5084656597c919d603a2d0346e15b790fefba0f8
Coco <tanmaig@hasura.io>
2016-11-04 02:21:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,6 +134,11 @@ app.get('/check-login', function (req, res) {
    }
 });
 
+app.get('/logout', function (req, res) {
+   delete req.session.auth;
+   res.send('Logged out');
+});
+
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
    // make a select request
