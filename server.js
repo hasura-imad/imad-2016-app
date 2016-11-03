61814934f3c83554f4498df665faf3d4d379a104
Coco <tanmaig@hasura.io>
2016-11-04 00:10:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user: 'coco98',
@@ -14,6 +15,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 function createTemplate (data) {
     var title = data.title;
@@ -69,6 +71,21 @@ app.get('/hash/:input', function(req, res) {
    res.send(hashedString);
 });
 
+app.post('/create-user', function (req, res) {
+   // username, password
+   // JSON
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.getRandomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send('User successfully created: ' + username);
+      }
+   });
+});
 
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
@@ -79,6 +96,7 @@ app.get('/test-db', function (req, res) {
           res.status(500).send(err.toString());
       } else {
           res.send(JSON.stringify(result.rows));
+          
       }
    });
 });
