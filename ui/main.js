7b3a45c2ddad19febba35e01061f8e7919c1b1b1
Coco <tanmaig@hasura.io>
2016-11-04 02:45:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,7 @@
 
 // Submit username/password to login
-var submit = document.getElementById('submit_btn');
+var submit = document.getElementById('login_btn');
 submit.onclick = function () {
-  
     // Create a request object
     var request = new XMLHttpRequest();
     
@@ -27,6 +26,35 @@ submit.onclick = function () {
     console.log(username);
     console.log(password);
     request.open('POST', 'http://coco98.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));  
+
+};
+
+var register = document.getElementById('register_btn');
+register.onclick = function () {
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // Take some action
+          if (request.status === 200) {
+              alert('User created successfully');
+          } else if (request.status === 500) {
+              alert('Could not register the user');
+          }
+      }  
+      // Not done yet
+    };
+    
+    // Make the request
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://coco98.imad.hasura-app.io/create-user', true);
     request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username: username, password: password}));  
 
