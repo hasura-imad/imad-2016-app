718abf333e60d450af0cfe870263c51a520af9b4
ramit0402 <ramitlegend@gmail.com>
2016-11-03 08:22:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var http = require("http");
+http.createServer(function (request, response) {
+ // Send the HTTP header
+ // HTTP Status: 200 : OK
+ // Content Type: text/plain
+ response.writeHead(200, {'Content-Type': 'text/plain'});
+
+ // Send the response body as "Hello World"
+ response.end('Hello World\n');
+}).listen(8081);
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
+
+
 var app = express();
 app.use(morgan('combined'));
 
