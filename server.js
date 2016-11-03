5bc2da92dba7d48c5977d101a14895681ee25685
Coco <tanmaig@hasura.io>
2016-11-04 03:25:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,15 +147,15 @@ app.get('/logout', function (req, res) {
 });
 
 var pool = new Pool(config);
-app.get('/test-db', function (req, res) {
+
+app.get('/get-articles', function (req, res) {
    // make a select request
    // return a response with the results
-   pool.query('SELECT * FROM test', function (err, result) {
+   pool.query('SELECT * FROM article ORDER BY date DESC', function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
           res.send(JSON.stringify(result.rows));
-          
       }
    });
 });
