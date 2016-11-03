f6bc3581a0a924f70e4b6f4381298e91248a639f
ramit0402 <ramitlegend@gmail.com>
2016-11-03 08:27:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -72,11 +79,6 @@ app.get('/images/music1.jpg', function (req, res) {
 
 app.get('/images/music5.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'images', 'music5.jpg'));
-});
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
 });
 
 app.get('/fonts/glyphicons-halflings-regular.eot', function (req, res) {
