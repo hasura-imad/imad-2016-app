86fd8734b40d01dfccecf4653d8ef6022b3e9787
akshatbhargava123 <akshatbhargavadeepa@gmail.com>
2016-11-06 00:53:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,30 +101,7 @@ app.get('/:articleName', function (req,res) {
 });
 
 
-app.get('/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/scripts/sweetalert.min.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'scripts/sweetalert.min.js'));
-});
-
-app.get('/css/sweetalert-master/dist/sweetalert.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'css/sweetalert-master/dist/sweetalert.css'));
-});
-
-app.get('/scripts/startup.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'scripts/startup.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
+app.use(express.static(path.join(__dirname, 'ui')));
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
