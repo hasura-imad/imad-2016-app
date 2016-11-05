f9277939213964d0499cd4420264e0a8890f3c4b
akshatbhargava123 <akshatbhargavadeepa@gmail.com>
2016-11-06 02:09:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ return htmlTemplate;
 }
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'startup.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'quiz.html'));
 });
 
 app.use(express.static(path.join(__dirname, 'ui')));
