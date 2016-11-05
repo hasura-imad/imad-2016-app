307d224088dce0ba88d4a3fc270e191553d9488d
akshatbhargava123 <akshatbhargavadeepa@gmail.com>
2016-11-06 02:15:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var articles = {
                  <p>
                     I am batman 
                 </p>`
-},
+    },
    'article-Two' : {
         title : 'Article Two : Gaurav Aggarwal',
         heading : 'Article Two',
@@ -52,50 +52,13 @@ var articles = {
     
 };
 
- function createTemplate(data){
-     var title = data.title;
-     var date = data.date;
-     var heading = data.heading;
-     var content = data.content;
-     var htmlTemplate = `
- <html>
-  <head>
-        <title>
-            ${heading} 
-        </title>
-        <meta name="viewport" content="width=device-width,initial-scale=1"/>
-          <link href="/ui/style.css" rel="stylesheet" />
-       
-    </head>
-    <body>
-        <div class="container">
-            <div>
-               <a href='/'>Home</a> 
-            </div>
-            <hr/>
-            <h3>
-                ${heading}
-            </h3>
-            <div>
-               ${date}
-            </div>
-            <div>
-               ${content}
-            </div>
-        </div>
-    </body>
-</html> `;
-
-return htmlTemplate;
-}
-
 app.use(express.static(path.join(__dirname, 'ui')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'quiz.html'));
 });
 
-var port = 8081; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8081, function () {
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
