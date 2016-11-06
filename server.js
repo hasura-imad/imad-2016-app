ca00e1c3fd83eb6847e43a4c966a55358e5a8eba
Prashanth Vaidya <vaiprash97@gmail.com>
2016-11-07 02:55:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 
 
