76af72c1fc1a61a19ed6ec0677ec3ab932d0bfa5
thecodearrow <vaiprash97@gmail.com>
2016-11-07 02:25:45 +0530

    mistake_rectify

--- server.js
+++ server.js
@@ -6,20 +6,12 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/resume.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'resume.html'));
 });
 
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui/css', 'style_resume.css'));
 });
 
 
