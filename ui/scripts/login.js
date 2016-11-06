c84643193acb425bbf0d6e1ee9e7a85a9941fb8d
akshatbhargava123 <akshatbhargavadeepa@gmail.com>
2016-11-06 08:33:55 +0530

    [imad-console] Updates ui/scripts/login.js

--- ui/scripts/login.js
+++ ui/scripts/login.js
@@ -1,11 +1,11 @@
-var users = ["user1","user2","user3","user4"]
-var passwords = ["123","132","312","213"]
+var users = ["user1","user2","user3","user4"];
+var passwords = ["123","132","312","213"];
 var totalUsers = 4;
 
 $(document).keypress(function(e) {
     if(e.which == 13) {
         // enter pressed
-        validateAll()
+        validateAll();
     }
 });
 
@@ -22,17 +22,17 @@ function validateAll() {
     request.open('GET', 'http://akshatbhargava123.imad.hasura-app.io/', true);
     request.send(null);
     
-    var inputname
-    var password
-    var statusSpan = document.getElementById('status')
-    var temp = document.getElementById('status-message')
-    var found = false
+    var inputname;
+    var password;
+    var statusSpan = document.getElementById('status');
+    var temp = document.getElementById('status-message');
+    var found = false;
     var userIndex;
     
     if(document.getElementById("username") !== "")
-        inputname = document.getElementById('username').value
+        inputname = document.getElementById('username').value;
     if(document.getElementById("password") !== "")
-        password = document.getElementById('password').value
+        password = document.getElementById('password').value;
     
     // SEARCH FOR USER IN USERS LIST
     for(var i = 0; i < totalUsers; i++) {
@@ -43,20 +43,20 @@ function validateAll() {
     }
     
     if(!found) {
-        temp.style.color = "#FF3408"
-        temp.innerHTML = "wrong username or password..."
-        statusSpan.style.display = 'block'
+        temp.style.color = "#FF3408";
+        temp.innerHTML = "wrong username or password...";
+        statusSpan.style.display = 'block';
     }
     else if(password !== passwords[userIndex]) {
-        temp.style.color = "#FF3408"
-        temp.innerHTML = "wrong username or password..."
-        statusSpan.style.display = 'block'
+        temp.style.color = "#FF3408";
+        temp.innerHTML = "wrong username or password...";
+        statusSpan.style.display = 'block';
     }
     else if (found){
-        temp.style.color = "#20FF00"
-        temp.innerHTML = "Logging you back..."
+        temp.style.color = "#20FF00";
+        temp.innerHTML = "Logging you back...";
         setTimeout(window.location.href = "user-work.html",2000);
-        statusSpan.style.display = 'block'
+        statusSpan.style.display = 'block';
     }
 }
 
