ddbed0e89401ae38095a31da3d330cd2616c70ac
Prashanth Vaidya <vaiprash97@gmail.com>
2016-11-07 02:46:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
