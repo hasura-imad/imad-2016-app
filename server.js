37562feacc36f01d8da12a7570a77bb1d3002c42
akshatbhargava123 <akshatbhargavadeepa@gmail.com>
2016-11-06 08:58:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.get('/users' , function(req, res){
         res.status(500).send(err.toString());
     } 
     else{
-        res.send(results.rows.toString());
+        res.send(JSON.stringify(results.rows));
     }
    });
 });
