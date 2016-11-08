1ad2a03f7960a7d3dd47cd892e489d1aeda03dc4
Prashanth Vaidya <vaiprash97@gmail.com>
2016-11-08 18:23:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,37 @@ app.get('/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
+app.get('/contact.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'contact.html'));
+});
+
+app.get('/follow.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'follow.html'));
+});
+
+app.get('/about.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'about.html'));
+});
+
+//images
+
+app.get('/dev.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'dev.jpg'));
+});
+
+app.get('/dev2.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'dev2.jpg'));
+});
+app.get('/dev3.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'dev3.jpg'));
+});
+app.get('/dev4.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'dev4.jpg'));
+});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
