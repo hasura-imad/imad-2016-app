eccdf7f6ecf6f630766d6ca4d11342b0a9d199f0
akshatbhargava123 <akshatbhargavadeepa@gmail.com>
2016-11-09 23:18:12 +0530

    [imad-console] Updates ui/scripts/quiz.js

--- ui/scripts/quiz.js
+++ ui/scripts/quiz.js
@@ -1,15 +1,19 @@
 //confirm('Ready for QUIZ? ')
 
+/*
 var questions = ['What is your name',
                 'Whats your age',
                 'How are you feeling?',
-                'How this page made?']
-var answers = ['name1','16','good','JS']
+                'How this page made?'];
+var answers = ['name1','16','good','JS'];
 var options = [
     ['name1','name2','name3','name4'],
     ['14','15','16','17'],
     ['awesome','good','bad','fine'],
-    ['JS','c++','c','ruby'] ]
+    ['JS','c++','c','ruby'] ];
+*/
+
+var questions = [], answers = [], options = [];
 
 var READY = false;
 var totalQuestions = 4;
@@ -22,6 +26,26 @@ var minutes = 0, seconds = 0;
 
 document.onreadystatechange = function () {
     if (document.readyState === "complete") { // PAGE HAS BEEN LOADED        
+        
+        var request = new XMLHttpRequest();
+        var object;
+        
+        request.onreadystatechange = function() {
+            if(request.readyState === XMLHttpRequest.DONE) {
+                // Take some action
+                if(request.status === 200) {
+                    object = JSON.parse(request.responseText);
+                    for(var i = 0; i < object.length; i++){
+                        users.push(object[i].name.toString());
+                        passwords.push(object[i].password.toString());
+                    }
+                }
+            }
+        };
+        
+        request.open('GET', 'http://akshatbhargava123.imad.hasura-app.io/', true);
+        request.send(null);
+        
         swal({
                 title: "Are you ready?",
                 text: "just a last confirmation...",
@@ -36,7 +60,7 @@ document.onreadystatechange = function () {
                 function(isConfirm) {
                     if (isConfirm) {
                         swal("Well...", "Good luck :)", "success");
-                        READY = true
+                        READY = true;
                     } else {
                         swal("Cancelled", "Its okay :)", "info");
                         swal(
@@ -45,34 +69,34 @@ document.onreadystatechange = function () {
                                 timer: 2000,
                                 showConfirmButton: true
                             },function(isConfirm){
-                                window.location.href = "user-work.html"
+                                window.location.href = "user-work.html";
                             }
-                        )
+                        );
                         //setTimeout(window.location.href = "user-work.html",20000)
                     }
                 }
             );
-        var newOptions = options[currentQuestion]
-        var newQuestion = questions[currentQuestion]
+        var newOptions = options[currentQuestion];
+        var newQuestion = questions[currentQuestion];
         var option = [
         document.getElementById("textOption1"),
         document.getElementById("textOption2"),
         document.getElementById("textOption3"),
-        document.getElementById("textOption4") ]
+        document.getElementById("textOption4") ];
         for(var i = 0; i < 4; i++){
-            option[i].innerHTML = newOptions[i]
+            option[i].innerHTML = newOptions[i];
         }
         var Question = document.getElementById('question');
         Question.innerHTML = newQuestion;
     }
-}
+};
 
 function getCheckedIndex() {
     var optionStatus = [
         document.getElementById("option1").checked,
         document.getElementById("option2").checked,
         document.getElementById("option3").checked,
-        document.getElementById("option4").checked ]
+        document.getElementById("option4").checked ];
     for(var i = 0; i < 4; i++){
         if(optionStatus[i]) { // true
             return i;
@@ -82,38 +106,38 @@ function getCheckedIndex() {
 }
 
 function updateOptions() {
-    var newOptions = options[currentQuestion]
-    var newQuestion = questions[currentQuestion]
+    var newOptions = options[currentQuestion];
+    var newQuestion = questions[currentQuestion];
     var option = [
     document.getElementById("textOption1"),
     document.getElementById("textOption2"),
     document.getElementById("textOption3"),
-    document.getElementById("textOption4") ]
+    document.getElementById("textOption4") ];
     for(var i = 0; i < 4; i++){
-        option[i].innerHTML = newOptions[i]
+        option[i].innerHTML = newOptions[i];
     }
     var Question = document.getElementById('question');
     Question.innerHTML = newQuestion;
 }
 
 function result(score, outOf) {
-    alert("You scored " + score + " out of " + outOf)
+    alert("You scored " + score + " out of " + outOf);
 }
 
 function next() {
-    var index = getCheckedIndex()
+    var index = getCheckedIndex();
     if(index !== -1){
         if(options[currentQuestion][index] === answers[currentQuestion]){
             correctAnswers++;
-            alert(correctAnswers)
+            alert(correctAnswers);
         }
         currentQuestion++;
     }
     if(currentQuestion === totalQuestions){
         //result(correctAnswers,totalQuestions)
-        alert("You scored " + correctAnswers + " out of " + totalQuestions)
+        alert("You scored " + correctAnswers + " out of " + totalQuestions);
     }
-    updateOptions()
+    updateOptions();
 }
 
 var intervalId = setInterval(function timer() {
@@ -125,11 +149,11 @@ var intervalId = setInterval(function timer() {
         }
         setInterval(function checkInterval() {
             if(minutes === 30){
-                clearInterval(intervalId)
-                alert('You scored' + correctAnswers + ' out of ' + totalQuestions)
+                clearInterval(intervalId);
+                alert('You scored' + correctAnswers + ' out of ' + totalQuestions);
             }
-        }, 1000)
-        var gameTimer = document.getElementById('timer')
-        gameTimer.innerHTML = 'TIME TAKEN YET : ' + (minutes < 10 ? 0 + ' ' + minutes : minutes) + ' : ' + (seconds < 10 ? 0 + ' ' + seconds : seconds)
+        }, 1000);
+        var gameTimer = document.getElementById('timer');
+        gameTimer.innerHTML = 'TIME TAKEN YET : ' + (minutes < 10 ? 0 + ' ' + minutes : minutes) + ' : ' + (seconds < 10 ? 0 + ' ' + seconds : seconds);
     }
-},1000)
+},1000);
