94d9e071a183bf4c9142b7e4a33f9eb8f3dfc9fa
akshatbhargava123 <akshatbhargavadeepa@gmail.com>
2016-11-09 23:47:41 +0530

    [imad-console] Updates ui/scripts/quiz.js

--- ui/scripts/quiz.js
+++ ui/scripts/quiz.js
@@ -1,80 +1,169 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-var Pool = require('pg').Pool;
+//confirm('Ready for QUIZ? ')
 
-var app = express();
-app.use(morgan('combined'));
+/*
+var questions = ['What is your name',
+                'Whats your age',
+                'How are you feeling?',
+                'How this page made?'];
+var answers = ['name1','16','good','JS'];
+var options = [
+    ['name1','name2','name3','name4'],
+    ['14','15','16','17'],
+    ['awesome','good','bad','fine'],
+    ['JS','c++','c','ruby'] ];
+*/
 
-var config = {    
-    user: 'gaurav00012',
-    database: 'gaurav00012',
-    password: 'db-gaurav00012-40955', // process.env.DB_PASSWORD
-    host: 'db.imad.hasura-app.io',
-    port: '5432'
+var questions = [], answers = [], options = [[]];
+
+var READY = false;
+var totalQuestions = 4;
+var totalOptions = 4;
+var correctAnswers = 0;
+var currentQuestion = 0;
+
+var minutes = 0, seconds = 0;
+
+var Quiz_Name = "Physics_Questions";
+
+document.onreadystatechange = function () {
+    if (document.readyState === "complete") { // PAGE HAS BEEN LOADED        
+        
+        var request = new XMLHttpRequest();
+        var object;
+        
+        // STRUCTURE OF INCOMING OBJECT
+        // ob = [{question,option1,...,option4,answer},...];
+        
+        request.onreadystatechange = function() {
+            if(request.readyState === XMLHttpRequest.DONE) {
+                // Take some action
+                if(request.status === 200) {
+                    object = JSON.parse(request.responseText);
+                    for(var i = 0; i < object.length; i++){
+                        questions.push(object[i].question.toString());
+                        answers.push(object[i].answer.toString());
+                        var options_object = [];
+                        for(var j = 1; j <= 4; j++) {
+                            options_object.push((object[i].option + j).toString());
+                            console.log(options_object[j]);
+                        }
+                        options.push(options_object);
+                    }
+                }
+            }
+        };
+        
+        request.open('GET', 'http://akshatbhargava123.imad.hasura-app.io/get-' + Quiz_Name , true);
+        request.send(null);
+        
+        swal({
+                title: "Are you ready?",
+                text: "just a last confirmation...",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonClass: "success",
+                confirmButtonText: "Yes",
+                closeOnConfirm: false,
+                //showLoaderOnConfirm = true,
+                closeOnCancel: false
+                },
+                function(isConfirm) {
+                    if (isConfirm) {
+                        swal("Well...", "Good luck :)", "success");
+                        READY = true;
+                    } else {
+                        swal("Cancelled", "Its okay :)", "info");
+                        swal(
+                            {
+                                title: "Redirecting to users' page...",
+                                timer: 2000,
+                                showConfirmButton: true
+                            },function(isConfirm){
+                                window.location.href = "user-work.html";
+                            }
+                        );
+                        //setTimeout(window.location.href = "user-work.html",20000)
+                    }
+                }
+            );
+        var newOptions = options[currentQuestion];
+        var newQuestion = questions[currentQuestion];
+        var option = [
+        document.getElementById("textOption1"),
+        document.getElementById("textOption2"),
+        document.getElementById("textOption3"),
+        document.getElementById("textOption4") ];
+        for(var i = 0; i < 4; i++){
+            option[i].innerHTML = newOptions[i];
+        }
+        var Question = document.getElementById('question');
+        Question.innerHTML = newQuestion;
+    }
 };
 
-var pool = new Pool(config);
-
-// [question, option1, ... , option4, answer][i]; //  0 = question, optionN = N, answer = length-1
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'startup.html'));
-});
-
-app.use(express.static(path.join(__dirname, 'ui')));
-
-// GETTING MATERIAL FOR QUIZ FROM DB
-app.get('/get-:table' , function(req, res) {
-    var tableName = req.params.table.toString();
-    var query = "SELECT * from \"" + tableName + "\""; 
-    pool.query(query, function(error, results){
-        if(error){
-            res.status(500).send(err.toString());
-        } else {
-            res.send(JSON.stringify(results.rows));
-        }
-    });
-});
-
-// GETTING LIST TO USERS (WORST PRACTISE NO SECURITY)
-app.get('/users' , function(req, res) {
-   pool.query('SELECT * from users', function(err, results) {
-    if(err){
-        res.status(500).send(err.toString());
-    }
-    else{
-        res.send(JSON.stringify(results.rows));
-    }
-   });
-});
-
-// FOR ADDING NEW USERS TO DB
-app.get('/add-:info' , function(req, res){
-    var info = req.params.info, temp;
-    var username = "", password ="";
-    for(var i = 0; i < info.length; i++){
-        if(info[i] !== '-'){
-            username += info[i];
-        } else{
-            temp = i;
-            break;
+function getCheckedIndex() {
+    var optionStatus = [
+        document.getElementById("option1").checked,
+        document.getElementById("option2").checked,
+        document.getElementById("option3").checked,
+        document.getElementById("option4").checked ];
+    for(var i = 0; i < 4; i++){
+        if(optionStatus[i]) { // true
+            return i;
         }
     }
-    for(i = temp + 1; i < info.length; i++){
-        password += info[i];
-    }
-    pool.query("INSERT INTO users (name, password) VALUES (" + "'" + username + "'" + "," + "'" + password + "'" + ");", function(err, results) {
-    if(err){
-        res.status(500).send(err.toString());
-    }
-    else{
-        res.send("User Registered!!!");
-    }
-   });
-});
+    return -1; // false
+}
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+function updateOptions() {
+    var newOptions = options[currentQuestion];
+    var newQuestion = questions[currentQuestion];
+    var option = [
+    document.getElementById("textOption1"),
+    document.getElementById("textOption2"),
+    document.getElementById("textOption3"),
+    document.getElementById("textOption4") ];
+    for(var i = 0; i < 4; i++){
+        option[i].innerHTML = newOptions[i];
+    }
+    var Question = document.getElementById('question');
+    Question.innerHTML = newQuestion;
+}
+
+function result(score, outOf) {
+    alert("You scored " + score + " out of " + outOf);
+}
+
+function next() {
+    var index = getCheckedIndex();
+    if(index !== -1){
+        if(options[currentQuestion][index] === answers[currentQuestion]){
+            correctAnswers++;
+            alert(correctAnswers);
+        }
+        currentQuestion++;
+    }
+    if(currentQuestion === totalQuestions){
+        //result(correctAnswers,totalQuestions)
+        alert("You scored " + correctAnswers + " out of " + totalQuestions);
+    }
+    updateOptions();
+}
+
+var intervalId = setInterval(function timer() {
+    if(READY){
+        seconds++;
+        if(seconds === 60){
+            minutes++;
+            seconds = 0;
+        }
+        setInterval(function checkInterval() {
+            if(minutes === 30){
+                clearInterval(intervalId);
+                alert('You scored' + correctAnswers + ' out of ' + totalQuestions);
+            }
+        }, 1000);
+        var gameTimer = document.getElementById('timer');
+        gameTimer.innerHTML = 'TIME TAKEN YET : ' + (minutes < 10 ? 0 + ' ' + minutes : minutes) + ' : ' + (seconds < 10 ? 0 + ' ' + seconds : seconds);
+    }
+},1000);
