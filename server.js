3924c24fc8c17f2b1a2b00e93517b7b63248a6a6
akshatbhargava123 <akshatbhargavadeepa@gmail.com>
2016-11-10 20:32:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
+/*
+                            WELCOME
+    The webapp is made by two students of the IMAD course, Ids' following:
+    Akshat - akshatbhargavadeepa@gmail.com
+    Gaurav - aggarwal.gaurav611@gmail.com
+    
+    Due to the tightness in daily schedule, the application has still a lot of
+    work not completed and some major bugs. (and yeah we know that getting users
+     data like that from DB :P , sorry but didn't got time to implement the true
+     work). ( FOR SURE BE ENJOYING EVERYTHING AGAIN IN OUR INTERNSHIP :D )
+    We have taken some references in Cascading Style Sheets,
+    whereas all the javascript work is totally done by us
+    without any kind of reference.
+    We have used three 3rd party JS libraries as follows:
+    1. Jquery
+    2. NotifyJS
+    3. SWeetAlert
+    
+    Regards
+    Akshat & Gaurav
+    9968516771
+    
+*/
+
+
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
