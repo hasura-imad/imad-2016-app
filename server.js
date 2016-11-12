2f1df6ce3248401af7574f40c87543704887bbcf
ramit0402 <ramitlegend@gmail.com>
2016-11-12 19:31:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ function createTemplate(data) {
 	<!--NAVBAR END-->
 
 	<!--CONATINER START-->
-	<div class="conatiner col-md-10 col-md-offset-1">
+	<div class="container col-md-10 col-md-offset-1">
 		<h3>${heading}</h3>
         <div>${date.toDateString()}</div>
         <div>${content}</div>
