55f126aa18929546ad1468b5f96f949c36ab54da
ramit0402 <ramitlegend@gmail.com>
2016-11-12 19:17:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,15 +68,14 @@ function createTemplate(data) {
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="index.html"><img src="images/logo_blog.png" alt="LOGO" ></a>
-				<!--<a class="navbar-brand" href="index.html"><img src="images/logo2.png" alt="LOGO" ></a>-->
+				<a class="navbar-brand" href="/"><img src="images/logo_blog.png" alt="LOGO" ></a>
 			</div>
 
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
 					<!--<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>-->----
-					<li><a id="home" href="#homeSection">Home</a></li>
+					<li><a id="home" href="/">Home</a></li>
 					<li><a id="article" href="#articleSection">My Articles</a></li>
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Browse <span class="caret"></span></a>
