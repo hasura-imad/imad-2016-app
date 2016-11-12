2fa87e49477a241f94c0c38fbe845086475737d9
AnandRajasekar <be15b004@smail.iitm.ac.in>
2016-11-12 15:16:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/anand.jpeg', function (req, res) {
+/*app.get('/ui/anand.jpeg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'anand.jpeg'));
-});
+});*/
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
