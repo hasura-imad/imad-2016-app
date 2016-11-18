ff7705d6d10ba0ed98e36ee0aa07c2e18a0ad8d1
golua94 <kumar.vikash863@gmail.com>
2016-11-18 12:16:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ return htmltemplate;}
 
 
 
-var pool=new pool(config);
+/*var pool=new pool(config);
 app.get('/test-db', function(req,res){
 pool.query("select * from Students", function(err,result){
    if(err){
@@ -106,7 +106,7 @@ pool.query("select * from Students", function(err,result){
    }
     
 });    
-});
+});*/
 
 
 app.get('/', function (req, res) {
