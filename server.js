0a6cea8994f1738206758e8e2399a03d175cda43
golua94 <kumar.vikash863@gmail.com>
2016-11-18 12:42:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ pool.query('SELECT * FROM Students', function(err,result){
    if(err){
        res.status(500).send(err.toString());
    } else{
-       res.send(JSON.stringify(result));
+       res.send(JSON.stringify(result.rows));
    }
     
 });    
