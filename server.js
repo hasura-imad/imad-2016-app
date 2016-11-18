cc94917b8727bc324e09f277fe4c9fd7a098ddbd
golua94 <kumar.vikash863@gmail.com>
2016-11-18 12:38:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get('/', function (req, res) {
 
 
 var pool=new pool(config);
-app.get('/test-db', function(req,res){
+app.get('/student-db', function(req,res){
 pool.query('SELECT * FROM test', function(err,result){
    if(err){
        res.status(500).send(err.toString());
