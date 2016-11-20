389e5fcee598f0f8e1968f12bbdfcb9d422669fc
golua94 <kumar.vikash863@gmail.com>
2016-11-20 20:10:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,14 +2,14 @@
 
 
 var button=document.getElementById('combu');
-var counter=0;
+
 button.onclick = function(){
      var request= new XMLHttpRequest();
       request.onreadystatechange=function(){
-        if(request.readyState == XMLHttpRequest.DONE){
+        if(request.readyState === XMLHttpRequest.DONE){
             
-            if(request.status == 200){
-                counter=request.responseText();
+            if(request.status === 200){
+               var counter=request.responseText();
                 var span=document.getElementById('comment');
                  span.innerHTML=counter.toString();
             }
