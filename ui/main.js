9b1bc8307de819a748f08d55e90f02605d65092c
golua94 <kumar.vikash863@gmail.com>
2016-11-20 19:45:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,25 +8,6 @@ button.onclick = function(){
     var span=document.getElementById('comment');
                  span.innerHTML=counter.toString();
     
-    
-    var request= new XMLHttpRequest();
-    
-    request.onreadystatechange=function(){
-        if(request.readyState == XMLHttpRequest.DONE){
-            
-            if(request.status == 200){
-                var counter= request.responseText;
-                alert("i am superhero");
-               
-                var span=document.getElementById('comment');
-                 span.innerHTML=counter.toString();
-            }
-        }
-    };
-   
-   request.open('GET', 'http://golua94.imad.hasura-app.io/ui/counter', true);
-   request.send(null);
-   
 };
 
  var nameInput=document.getElementById('name');
