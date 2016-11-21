c37af602f0c8bee7c4b7aeb0ae2ebaeb3ed662cf
suryan123 <suryanprasadom@gmail.com>
2016-11-21 14:06:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,21 +1,6 @@
 
 
 
-
-
-
-website_folder/
-   index.html <- this is the homepage
-   faq/
-     index.html <- this is served from "/faq" and from "/faq/index.html"
-   contact/
-     index.html <- this is served from "/contact" and "/contact/index.html"
-     
-     
-     
-    
-
-
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
