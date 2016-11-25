d24f19e4dbddf3a1b1e12fa9f6ea1b9fda62bc2b
suryan123 <suryanprasadom@gmail.com>
2016-11-25 16:55:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,41 +7,65 @@ app.use(morgan('combined'));
 
 var article={
     'article-one': {
-        title:'article one | manoj duli',
+        title:'article one | suryan123',
         heading:'article one',
         date:'19 th november 2016',
         content:`
             <p>
-                    HAI THIS IS "MANOJ DULI"THIS IS MY FIRST ARTICLE. HAI THIS IS "MANOJ DULI" THIS IS MY FIRST ARTICLE.HAI THIS IS "MANOj DULI"THIS IS MY FIRST ARTICLE. HAI THIS IS "MANOJ DULI"THIS IS MY FIRST ARTICLE.
-            </p>
-                                 
-            <p>
-                    HAI THIS IS "MANOJ DULI"THIS IS MY FIRST ARTICLE. HAI THIS IS "MANOJ DULI" THIS IS MY FIRST ARTICLE.HAI THIS IS "MANOJ DULI"THIS IS MY FIRST ARTICLE. HAI THIS IS "MANOJ DULI"THIS IS MY FIRST ARTICLE.
-                        
-            </p>
-            <p>
-                    HAI THIS IS "MANOJ DULI"THIS IS MY FIRST ARTICLE. HAI THIS IS "MANOJ DULI" THIS IS MY FIRST ARTICLE.HAI THIS IS "MANOJ DULI"THIS IS MY FIRST ARTICLE. HAI THIS IS "MANOJ DULI"THIS IS MY FIRST ARTICLE.
+                    THIS IS MY FIRST ARTICLE.
+           THIS IS MY FIRST ARTICLE.
             </p>` 
                 
                 
     },
     'article-two': {
-        title:'article two | manoj duli',
+        title:'article two | suryan123',
             heading:'article two',
             date:'15 th november 2016',
             content:`
                 <p>
-                    HAI THIS IS "MANOJ DULI"THIS IS MY SECOND ARTICLE. HAI THIS IS "MANOJ DULI" THIS IS MY SECOND ARTICLE.
+                    HAI THIS IS THIS IS MY SECOND ARTICLE. 
                 </p>`
        },
     'article-three': {
-        title:'article two | manoj duli',
+        title:'article two | suryan123',
             heading:'article two',
             date:'11 th november 2016',
             content:`
                 <p>
-                    HAI THIS IS "MANOJ DULI"THIS IS MY SECOND ARTICLE. HAI THIS IS "MANOJ DULI" THIS IS MY SECOND ARTICLE.
+                    HAI THIS IS THIS IS MY SECOND ARTICLE.
                 </p>`
+                
+                
+                
+                
+                
+    },
+    'article-four': {
+        title:'Article Four | Suryan',
+            heading:'Article four',
+            date:'11 th november 2016',
+            content:`
+                <p>
+                    HAI THIS IS THIS IS MY fourth ARTICLE.
+                </p>`
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
    
     
     }
@@ -66,11 +90,11 @@ function createTemplate(data){
          <div>
              <a href="/">Home</a>
              <span style="display:inline-block; width: 100;"></span>
-             <a href="/article-one">ARTICLE ONE</a>
+             <a href="/article-one">1</a>
              <span style="display:inline-block; width: 100;"></span>
-             <a href="/article-two">ARTICLE TWO</a>
+             <a href="/article-two">2</a>
              <span style="display:inline-block; width: 100;"></span>
-             <a href="/article-three">ARTICLE THREE</a>
+             <a href="/article-three">3</a>
          </div>
          <h3>
             ${heading}
