fde347a0088706ff4a290255e452ff832114540c
suryan123 <suryanprasadom@gmail.com>
2016-11-25 17:49:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-var app = express();
-app.use(morgan('combined'));
+
 
 var article={
     'article-one': {
