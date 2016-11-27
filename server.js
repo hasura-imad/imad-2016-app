35a30da138f185f9153c45286e4127479f6d3a6e
golua94 <kumar.vikash863@gmail.com>
2016-11-27 11:46:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var session = require('express-session');
 var config={
     user : 'golua94',
     database :'golua94',
-    host:'db.imad.hasura.io',
+    host:'db.imad.hasura-app.io',
     port:'5432',
     password:process.env.DB_PASSWORD
 };
