e87b527d555462d17224f8c648c2219b86780b89
srinivasanpalani <psrisrinivasan96@gmail.com>
2016-11-29 16:08:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
+app.get('/article1', function (req, res) {
   res.sendFile('article one is served here');
 });
 
