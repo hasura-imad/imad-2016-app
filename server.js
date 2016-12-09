1dc61c2a5ffe2b215055f8e1120e760f2eaa7d3d
suryan123 <suryanprasadom@gmail.com>
2016-12-09 18:35:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -322,7 +322,48 @@ p {
 <hr>
 <br>
             
-            
+    <h3>4.</h3>
+    <h2>CSS Links</h3>
+    
+    <html>
+<head>
+<style>
+/* unvisited link */
+a:link {
+    color: red;
+}
+
+/* visited link */
+a:visited {
+    color: green;
+}
+
+/* mouse over link */
+a:hover {
+    color: hotpink;
+}
+
+/* selected link */
+a:active {
+    color: blue;
+}
+</style>
+</head>
+<body>
+
+<p><b><a href="http://suryan123.imad.hasura-app.io" target="_blank">This is a link</a></b></p>
+<p><b>Note:</b> a:hover MUST come after a:link and a:visited in the CSS definition in order to be effective.</p>
+<p><b>Note:</b> a:active MUST come after a:hover in the CSS definition in order to be effective.</p>
+
+</body>
+</html>
+
+
+<p><a href="http://www.w3schools.com/css/tryit.asp?filename=trycss_link"><button>Click here to view Code</button></a></p>
+<hr>
+<br>
+
+
             
             
             
