6c725f74b1de23eb43c02db98fd22a878e3e23c0
suryan123 <suryanprasadom@gmail.com>
2016-12-09 18:38:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -363,6 +363,76 @@ a:active {
 <hr>
 <br>
 
+<h3>5.</h3>
+<h2>Using CSS to style a HTML Form</h2>
+
+<!DOCTYPE html>
+<html>
+<style>
+input[type=text], select {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+
+input[type=submit] {
+    width: 100%;
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+input[type=submit]:hover {
+    background-color: #45a049;
+}
+
+div {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+}
+</style>
+<body>
+
+<h3>Using CSS to style a HTML Form</h3>
+
+<div>
+  <form action="action_page.php">
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname">
+
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lastname">
+
+    <label for="country">State</label>
+    <select id="country" name="country">
+      <option value="australia">Australia</option>
+      <option value="canada">Canada</option>
+      <option value="usa">USA</option>
+    </select>
+  
+    <input type="submit" value="Submit">
+  </form>
+</div>
+
+</body>
+</html>
+
+<p><a href="http://www.w3schools.com/css/css_form.asp"><button>Click here to view Code</button></a></p>
+
+<hr>
+<br>
+
+
+
 
             
             
